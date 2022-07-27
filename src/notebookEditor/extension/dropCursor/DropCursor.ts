eb553ece88659e6d9a1db61acc4559ca898d5560
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-27 13:15:21 -0500

    Consistency

--- src/notebookEditor/extension/dropCursor/DropCursor.ts
+++ src/notebookEditor/extension/dropCursor/DropCursor.ts
@@ -7,7 +7,7 @@ import { ExtensionName } from 'notebookEditor/model/type';
 // == Extension ===================================================================
 export interface DropCursorOptions { color: string | null; width: number | null; class: string | null; }
 export const DropCursor = Extension.create<DropCursorOptions>({
-  name: ExtensionName.DROP_CURSOR/*Expected and guaranteed to be unique.*/,
+  name: ExtensionName.DROP_CURSOR/*Expected and guaranteed to be unique*/,
 
   // -- Attribute -----------------------------------------------------------------
   addOptions() { return { color: 'currentColor', width: 1, class: null }; },
