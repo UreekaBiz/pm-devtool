4ff68e86e964d846d3da30933f31da004a04c044
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-27 12:57:45 -0500

    Consistency and brevity

--- src/notebookEditor/toolbar/ToolbarContainer.tsx
+++ src/notebookEditor/toolbar/ToolbarContainer.tsx
@@ -1,9 +1,7 @@
 import { Flex, FlexProps, Text } from '@chakra-ui/react';
 
 // ********************************************************************************
-interface Props extends FlexProps {
-  name: string;
-}
+interface Props extends FlexProps { name: string; }
 export const ToolContainer: React.FC<Props> = ({ name, width, marginTop, children }) =>
   <Flex flexDir='column' width={width} marginTop={marginTop}>
     <Text fontSize='14px'>{name}</Text>
