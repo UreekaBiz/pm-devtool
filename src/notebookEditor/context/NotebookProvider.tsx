34dd63b929ad4c32657177f4a5e596da5617c26f
ErickJoestar <erickborquez@gmail.com>
2022-07-27 14:35:48 -0500

    Sync Theme Fix

--- src/notebookEditor/context/NotebookProvider.tsx
+++ src/notebookEditor/context/NotebookProvider.tsx
@@ -1,5 +1,7 @@
 import { useEditor } from '@tiptap/react';
+import { useEffect } from 'react';
 
+import { notebookEditorTheme } from 'notebookEditor/extension/theme/theme';
 import { editorDefinition } from 'notebookEditor/type';
 
 import { NotebookContext } from './NotebookContext';
@@ -8,6 +10,11 @@ import { NotebookContext } from './NotebookContext';
 interface Props { children: React.ReactNode; }
 export const NotebookProvider: React.FC<Props> = ({ children }) => {
   const editor = useEditor(editorDefinition);
+
+  // sets the initial theme when the component mounts
+  useEffect(() => {
+    notebookEditorTheme.setThemeStylesheet()/*sync stylesheet*/;
+  }, []);
 
   return <NotebookContext.Provider value={{ editor }}>{children}</NotebookContext.Provider>;
 };
