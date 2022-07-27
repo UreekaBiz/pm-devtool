dcc366e0de25ec91ff029481ec5f4c5c7b6eb3d4
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-27 12:57:21 -0500

    Consistency

--- src/notebookEditor/toolbar/toolbar.ts
+++ src/notebookEditor/toolbar/toolbar.ts
@@ -7,8 +7,8 @@ import { ParagraphToolbar } from 'notebookEditor/extension/paragraph/toolbar';
 import { Toolbar } from './type';
 
 // ********************************************************************************
-// A collection of toolbars. Each Node can have its own toolbar, if its not defined
-// in the collection nothing will be shown.
+// A collection of Toolbars. Each Node can have its own Toolbar. If it's not defined
+// in the collection then nothing will be shown.
 const TOOLBAR_MAP: Record<NodeName, Toolbar | null> = {
   [NodeName.DOC]: DocumentToolbar,
   [NodeName.TEXT]: null/*none*/,
@@ -16,10 +16,10 @@ const TOOLBAR_MAP: Record<NodeName, Toolbar | null> = {
   [NodeName.HEADING]: HeadingToolbar,
 };
 
+// --------------------------------------------------------------------------------
 /**
- * Gets the corresponding toolbar for a given node
  * @param nodeName The name of the node whose toolbar is being asked for
- * @returns The corresponding toolbar object for the given node name
+ * @returns The corresponding Toolbar for the given Node name
  */
 export const getToolbar = (nodeName: NodeName): Toolbar | null => {
   let toolbar = TOOLBAR_MAP[nodeName];
