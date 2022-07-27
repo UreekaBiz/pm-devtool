01537027ec2452354bc6aee6879b3058bd3e96a7
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-27 09:06:20 -0500

    Simplified

--- src/notebookEditor/extension/util/mark.ts
+++ src/notebookEditor/extension/util/mark.ts
@@ -3,21 +3,19 @@ import { MarkType } from 'prosemirror-model';
 
 // ********************************************************************************
 /**
- * Utility function to create an {@link InputRule} without {@link RegExp} duplication
- * Returns a {@link InputRule} for the given {@link MarkType}
  * @param regExp The {@link RegExp} that will be turned into an {@link InputRule} for the desired {@link MarkType}
  * @param type The {@link MarkType} for which a new {@link InputRule} will be created
- * @returns The created {@link InputRule} for the corresponding {@link MarkType}
- * REF: https://tiptap.dev/guide/custom-extensions
+ * @returns an {@link InputRule} for the corresponding {@link MarkType} without
+ *          {@link RegExp} duplication
  */
+// REF: https://tiptap.dev/guide/custom-extensions
 export const markInputRule = (regExp: RegExp, type: MarkType): InputRule => tiptapMarkInputRule({ find: new RegExp(regExp.source + '$'), type });
 
 /**
- * Utility function to create an {@link PasteRule} without {@link RegExp} duplication
- * Returns a {@link PasteRule} for the given {@link MarkType}
  * @param regExp The {@link RegExp} that will be turned into an {@link PasteRule} for the desired {@link MarkType}
  * @param type The {@link MarkType} for which a new {@link PasteRule} will be created
- * @returns The created {@link PasteRule} for the corresponding {@link MarkType}
- * REF: https://tiptap.dev/guide/custom-extensions
+ * @returns a {@link PasteRule} for the corresponding {@link MarkType} without
+ *          {@link RegExp} duplication
  */
+// REF: https://tiptap.dev/guide/custom-extensions
 export const markPasteRule = (regExp: RegExp, type: MarkType): PasteRule => tiptapMarkPasteRule({ find: new RegExp(regExp.source, 'g'), type });
