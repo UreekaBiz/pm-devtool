bbe7062d0d904f0e3cc3e342cdd82e0fd45cf650
Rob Grzywinski <rob.grzywinski@ureeka.biz>
2022-07-29 09:12:37 -0500

    FIXME and consistency

--- src/notebookEditor/model/type.ts
+++ src/notebookEditor/model/type.ts
@@ -25,11 +25,11 @@ export enum ExtensionName {
 }
 
 // == Priority ====================================================================
-// NOTE: priority can affect extensions, Nodes and Marks
+// NOTE: priority can affect Extensions, Nodes and Marks
 
 // -- Extension -------------------------------------------------------------------
-// NOTE: if extension priority is left unspecified, it defaults to 100
-// NOTE: names match extension, Node or Mark names for sanity.
+// NOTE: if Extension priority is left unspecified, it defaults to 100
+// NOTE: names match Extension, Node or Mark names for sanity
 export enum ExtensionPriority {
   // -- Extension -----------------------------------------------------------------
   UNIQUE_NODE_ID = 120/*T&E*/,
@@ -40,24 +40,25 @@ export enum ExtensionPriority {
   // NOTE: Paragraph must have a higher priority than other block Nodes since it
   //       is the 'default' block Node (by convention). If its priority is left
   //       unspecified, the default block Node on document creation will be the
-  //       first block Node encountered in the editor extension array
-  //       (SEE: notebookEditor/type.ts)
+  //       first block Node encountered in the editor Extension array
+  // SEE: notebookEditor/type.ts
   PARAGRAPH = 117,
 
   // NOTE: Paragraph must have a higher priority than other block Nodes since it
   //       is the 'default' block Node (by convention). If its priority is left
   //       unspecified, the default block Node on document creation will be the
-  //       first block Node encountered in the editor extension array
-  //       (SEE: notebookEditor/type.ts)
+  //       first block Node encountered in the editor Extension array
+  // FIXME: this was copied from above and not changed!!!
+  // SEE: notebookEditor/type.ts
   TEXT_BLOCK = 116,
 
 
-  // NOTE: Since the text extension adds a \t whenever Tab is pressed, but this
+  // NOTE: since the Text Extension adds '\t' whenever Tab is pressed, but this
   //       behavior is not always guaranteed to be the desired one (e.g. when
-  //       going through a list Node), the text extension runs last (SEE: note
-  //       above for default extension priority). This ensures that the shortcuts
-  //       defined in the text extension run only if their trigger was not handled
-  //       by another extension previously
+  //       going through a list Node), the Text Extension runs last. This ensures
+  //       that the shortcuts defined in the Text Extension run only if their
+  //       trigger was not handled by another Extension previously
+  // SEE: NOTE above for default Extension priority
   TEXT = 99,
 
   // -- Mark ----------------------------------------------------------------------
@@ -65,10 +66,11 @@ export enum ExtensionPriority {
 }
 
 // NOTE: if parse rule priority is left unspecified, it defaults to 50
-// NOTE: names match extension, Node or Mark names for sanity.
+// NOTE: names match Extension, Node or Mark names for sanity
 export enum ParseRulePriority {/*currently nothing*/}
 
 // == Selection ===================================================================
-// the depth of the selection from the current Node. 0 is the base Node,
-// `selection.depth` is the parent Node.
+// the depth of the selection from the current Node:
+// * 0 is the base Node
+// * `selection.depth` is the parent Node
 export type SelectionDepth = number | undefined/*current Node*/;
