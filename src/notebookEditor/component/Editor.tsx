7d13b1960b4675e07a530f2b52f033f757242333
Alan Rodz <s061199@hotmail.com>
2022-08-10 17:58:16 -0500

    add isActionModifierPressed check

--- src/notebookEditor/component/Editor.tsx
+++ src/notebookEditor/component/Editor.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@chakra-ui/react';
 import { EditorContent } from '@tiptap/react';
+import { useEffect, useState } from 'react';
 
 import { useNotebookEditor } from 'notebookEditor/hook/useNotebookEditor';
 
@@ -7,9 +8,37 @@ import { EditorUserInteractions } from './EditorUserInteractions';
 
 // ********************************************************************************
 export const EDITOR_CONTAINER_ID = 'NotebookEditorContainerID';
+const EDITOR_ACTIONABLE_CLASS = 'Editor-actionable';
 
 export const Editor: React.FC = () => {
   const { editor } = useNotebookEditor();
+
+  // == State =====================================================================
+  const [isActionModifierPressed, setIsActionModifierPressed] = useState(false);
+
+  // == Effect ====================================================================
+  // add the actionable class to the Editor if the CMD or the CTRL keys are
+  // pressed, which will make actionable nodes have special styles (SEE: index.css)
+  useEffect(() => {
+    const setActionableClass = (event: KeyboardEvent) => {
+      if(event.ctrlKey || event.metaKey) {
+        setIsActionModifierPressed(true);
+      } /* else -- do not set to true */
+    };
+    const unsetActionableClass = (event: KeyboardEvent) => {
+      if(!(event.ctrlKey) && !(event.metaKey)) {
+        setIsActionModifierPressed(false);
+      } /* else -- do not set to false */
+    };
+
+    window.addEventListener('keydown', setActionableClass);
+    window.addEventListener('keyup', unsetActionableClass);
+
+    return () => {
+      window.removeEventListener('keydown', unsetActionableClass);
+      window.removeEventListener('keyup', unsetActionableClass);
+    };
+  }, []);
 
   // == Handler ===================================================================
   const handleClick = () => {
@@ -21,7 +50,7 @@ export const Editor: React.FC = () => {
 
   // == UI ========================================================================
   return (
-    <Box id={EDITOR_CONTAINER_ID} height='full' overflowY='auto' onClick={handleClick}>
+    <Box id={EDITOR_CONTAINER_ID} className={isActionModifierPressed ? EDITOR_ACTIONABLE_CLASS : ''} height='full' overflowY='auto' onClick={handleClick}>
       <EditorUserInteractions />
       <EditorContent editor={editor} />
     </Box>
