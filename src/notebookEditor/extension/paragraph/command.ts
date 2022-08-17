a3b829fb4a1f6a0396596889fbd47d3592227a98
Alan Rodz <s061199@hotmail.com>
2022-08-17 13:52:57 -0500

    maintain selection with Paragraph command

--- src/notebookEditor/extension/paragraph/command.ts
+++ src/notebookEditor/extension/paragraph/command.ts
@@ -1,7 +1,7 @@
 import { CommandProps } from '@tiptap/core';
-import { TextSelection } from 'prosemirror-state';
+import { NodeSelection, TextSelection } from 'prosemirror-state';
 
-import { createParagraphNode, getBlockNodeRange, CommandFunctionType, NodeName } from 'common';
+import { createParagraphNode, getBlockNodeRange, isNodeSelection, CommandFunctionType, NodeName } from 'common';
 
 // ********************************************************************************
 // NOTE: ambient module to ensure command is TypeScript-registered for TipTap
@@ -17,16 +17,28 @@ declare module '@tiptap/core' {
 // --------------------------------------------------------------------------------
 export const setParagraphCommand = () => ({ tr, dispatch, view }: CommandProps) => ((tr, dispatch, view) => {
   const { from, to } = getBlockNodeRange(tr.selection);
-  const textContent = tr.doc.textBetween(from, to, '\n'/*insert for every Block Node*/);
   const resolvedFrom = tr.doc.resolve(from - 1/*select the whole parent*/),
         resolvedTo = tr.doc.resolve(to + 1/*select the whole parent*/);
+
+  const textContent = tr.doc.textBetween(from, to, '\n'/*insert for every Block Node*/);
+
+  const startingSelection = tr.selection;
 
   // NOTE: this is specifically replacing the selection instead of calling
   //       setBlockType to account for the case where the Paragraph's only
   //       Content is a MarkHolder Node, which gets deleted by the
   //       command (effectively removing Marks on next input)
   tr.setSelection(new TextSelection(resolvedFrom, resolvedTo))
-    .replaceSelectionWith(createParagraphNode(view?.state.schema, undefined/*do not specify attrs*/, textContent.length > 0 ? view?.state.schema.text(textContent) : undefined/*no content*/));
+    .replaceSelectionWith(createParagraphNode(view?.state.schema, undefined/*do not specify attrs*/, textContent.length > 0 ? view?.state.schema.text(textContent) : undefined/*no content*/))
+    .setSelection(
+      isNodeSelection(startingSelection)
+      ? new NodeSelection(tr.doc.resolve(startingSelection.$anchor.pos))
+      : new TextSelection(tr.doc.resolve(startingSelection.$anchor.pos), tr.doc.resolve(startingSelection.$head.pos))
+    );
+
+  // if(!isParagraphNode(tr.selection.$anchor.parent/*guaranteed to be same pos as head by replaceSelectionWith call above*/)) {
+  //   const insertedParagraphPos = Math.max(1/*do not go behind start of doc*/, tr.selection.$anchor.pos - 2/*at the end of the new Paragraph*/);
+  // }
 
   if(dispatch) dispatch(tr);
   return true/*can be executed*/;
