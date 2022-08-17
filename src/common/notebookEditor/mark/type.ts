32e97b998d7e22a59a5228e58ec78f263c282ef5
Alan Rodz <s061199@hotmail.com>
2022-08-17 13:25:31 -0500

    add and use markFromJSONMark

--- src/common/notebookEditor/mark/type.ts
+++ src/common/notebookEditor/mark/type.ts
@@ -1,6 +1,7 @@
 import { Mark as ProseMirrorMark } from 'prosemirror-model';
 
 import { Attributes } from '../attribute';
+import { NotebookSchemaType } from '../schema';
 
 // ********************************************************************************
 export type JSONMark<A extends Attributes = {}> = {
@@ -19,5 +20,6 @@ export enum MarkName {
 export const getMarkName = (mark: ProseMirrorMark) => mark.type.name as MarkName;
 
 // --------------------------------------------------------------------------------
+export const markFromJSONMark = (schema: NotebookSchemaType, jsonMark: JSONMark) => ProseMirrorMark.fromJSON(schema, jsonMark);
 export const stringifyMarksArray = (marks: ProseMirrorMark[]) => JSON.stringify(marks);
 export const parseStringifiedMarksArray = (stringifiedMarks: string) => JSON.parse(stringifiedMarks) as JSONMark[]/*FIXME: handle exceptions!!!*/;
