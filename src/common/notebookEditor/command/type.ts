721c1793680027fe0f2b3eed7caaa66e5d83acc0
Alan Rodz <s061199@hotmail.com>
2022-08-19 19:15:02 -0500

    change type

--- src/common/notebookEditor/command/type.ts
+++ src/common/notebookEditor/command/type.ts
@@ -2,4 +2,4 @@ import { Transaction } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 
 // ********************************************************************************
-export type Command = (tr: Transaction, dispatch?: (tr: Transaction) => void, view?: EditorView) => boolean/*indicates whether the command can be performed*/;
+export type Command = (tr: Transaction, dispatch: (tr: Transaction) => void, view: EditorView) => boolean/*indicates whether the command can be performed*/;
