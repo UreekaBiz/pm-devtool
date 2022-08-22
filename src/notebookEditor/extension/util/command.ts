d187eeea9e05f4f2b2bbbd53c05846d8180757fd
Alan Rodz <s061199@hotmail.com>
2022-08-22 15:11:39 -0500

    change doc

--- src/notebookEditor/extension/util/command.ts
+++ src/notebookEditor/extension/util/command.ts
@@ -1,6 +1,5 @@
 import { Editor } from '@tiptap/core';
 
 // ********************************************************************************
-// returns the props needed for a Command.
-// Located in web since Editor comes from TipTap
+// returns the props needed for a Command
 export const getCommandProps = (editor: Editor) => ({ state: editor.state, dispatch: editor.view.dispatch, view: editor.view });
