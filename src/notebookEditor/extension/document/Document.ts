d5580900e33eb409af40070d4b874a3900d1d07d
Alan Rodz <s061199@hotmail.com>
2022-08-23 17:41:00 -0500

    WIP: text only paste in doc through plugin

--- src/notebookEditor/extension/document/Document.ts
+++ src/notebookEditor/extension/document/Document.ts
@@ -1,10 +1,10 @@
 import { Node } from '@tiptap/core';
-import { Plugin } from 'prosemirror-state';
-import { EditorView } from 'prosemirror-view';
 
-import { DocumentNodeSpec, NotebookSchemaType } from 'common';
+import { DocumentNodeSpec } from 'common';
 
 import { NoOptions, NoStorage } from 'notebookEditor/model/type';
+
+import { DocumentPlugin } from './plugin';
 
 // ********************************************************************************
 // REF: https://github.com/ueberdosis/tiptap/blob/main/packages/extension-document/src/document.ts
@@ -14,23 +14,7 @@ export const Document = Node.create<NoOptions, NoStorage>({
   ...DocumentNodeSpec,
 
   // -- Plugin --------------------------------------------------------------------
-  // REF: https://discuss.prosemirror.net/t/disable-ctrl-click/995/2
-  // NOTE: this Plugin prevents the default parent Node selection behavior
-  //      from being triggered when either the CMD or the CTRL keys are pressed.
-  //      This default behavior comes from PM (SEE: REF above)
-  addProseMirrorPlugins() {
-    return [
-      new Plugin<NotebookSchemaType>({
-        props: {
-          handleClick(view: EditorView, pos: number, event: MouseEvent) {
-            if(event.ctrlKey || event.metaKey) return true/*(SEE: comment above)*/;
-
-            return false/*allow regular event handling*/;
-          },
-        },
-      }),
-    ];
-  },
+  addProseMirrorPlugins() { return [DocumentPlugin()]; },
 
   // -- Keyboard Shortcut ---------------------------------------------------------
   // REF: https://prosemirror.net/docs/ref/#commands.pcBaseKeymap
