30e8bca1fcabb04f22c700df0e84d24a09f7c050
Alan Rodz <s061199@hotmail.com>
2022-08-29 17:57:33 -0500

    modify insertTabCommand

--- src/notebookEditor/extension/text/command.ts
+++ src/notebookEditor/extension/text/command.ts
@@ -1,11 +1,22 @@
-import { Command } from 'common';
+import { EditorState, Transaction } from 'prosemirror-state';
+
+import { AbstractDocumentUpdate, Command } from 'common';
 
 // ********************************************************************************
 /** Inserts a Tab. (SEE: ExtensionPriority) for details on handling */
 export const insertTabCommand: Command = (state, dispatch) => {
-  const { tr } = state;
-  tr.insertText('\t');
-
-  dispatch(tr);
-  return true/*Command executed*/;
+  const updatedTr = new InsertTabDocumentUpdate().update(state, state.tr);
+  dispatch(updatedTr);
+  return true/*command executed*/;
 };
+export class InsertTabDocumentUpdate implements AbstractDocumentUpdate {
+  public constructor() {/*nothing additional*/}
+
+  /**
+   * modify the given Transaction such that a Tab is inserted and return it
+   */
+  public update(editorState: EditorState, tr: Transaction) {
+    tr.insertText('\t');
+    return tr;
+  }
+}
