d7e68c5133b2a3fd442d05397e7b8d3a2c4cb856
Alan Rodz <s061199@hotmail.com>
2022-09-06 20:40:42 -0500

    add rules

--- src/notebookEditor/extension/document/Document.ts
+++ src/notebookEditor/extension/document/Document.ts
@@ -1,4 +1,4 @@
-import { Node } from '@tiptap/core';
+import { textInputRule, Node } from '@tiptap/core';
 
 import { DocumentNodeSpec } from 'common';
 
@@ -8,6 +8,13 @@ import { DocumentPlugin } from './plugin';
 
 // ********************************************************************************
 // REF: https://github.com/ueberdosis/tiptap/blob/main/packages/extension-document/src/document.ts
+
+// == Constant ====================================================================
+// a map of strings that, when typed, get replaced by a specific character
+const documentInputRules: { find: RegExp; replace: string; }[] = [
+  { find: /<-$/, replace: '←' },
+  { find: /->$/, replace: '→' },
+];
 
 // == Node ========================================================================
 export const Document = Node.create<NoOptions, NoStorage>({
@@ -25,4 +32,7 @@ export const Document = Node.create<NoOptions, NoStorage>({
   //       other Nodes at a Document level, return true
   //       (preventing the default behavior)
   addKeyboardShortcuts() { return { 'Mod-Enter': () => true/*do not let PM handle the shortcut*/ }; },
+
+  // -- Input ---------------------------------------------------------------------
+  addInputRules() { return documentInputRules.map(({ find, replace }) => textInputRule({ find, replace })); },
 });
