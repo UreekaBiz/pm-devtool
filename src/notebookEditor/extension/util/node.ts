6fd8fd95e481bd7e6476fff61c75cf1678f706ab
Alan Rodz <s061199@hotmail.com>
2022-09-26 12:37:28 -0500

    match notebook

--- src/notebookEditor/extension/util/node.ts
+++ src/notebookEditor/extension/util/node.ts
@@ -2,7 +2,7 @@ import { Editor } from '@tiptap/core';
 import { DOMParser, Fragment, Node as ProseMirrorNode, ParseOptions } from 'prosemirror-model';
 import { GapCursor } from 'prosemirror-gapcursor';
 
-import { isGapCursorSelection, JSONNode, NodeName, NotebookSchemaType } from 'common';
+import { clearNodesCommand, isGapCursorSelection, JSONNode, NodeName, NotebookSchemaType } from 'common';
 
 import { elementFromString } from './parse';
 
@@ -57,7 +57,7 @@ export const handleBlockBackspace = (editor: Editor, nodeName: NodeName) => {
 
   if(!empty || $anchor.parent.type.name !== nodeName) return false/*let event be handled elsewhere*/;
   if(isAtStartOfDoc || !$anchor.parent.textContent.length) {
-    return editor.commands.clearNodes();
+    return clearNodesCommand(editor.state, editor.view.dispatch);
   } /* else -- no need to delete blockNode */
 
   return false/*let event be handled elsewhere*/;
