11d41c68778ffc5c7975788f5bf95699ff910143
Alan Rodz <s061199@hotmail.com>
2022-10-03 15:51:27 -0500

    add command

--- src/common/notebookEditor/command/selection.ts
+++ src/common/notebookEditor/command/selection.ts
@@ -3,6 +3,7 @@ import { EditorState, NodeSelection, Selection, TextSelection, Transaction } fro
 import { EditorView } from 'prosemirror-view';
 
 import { minFromMax } from '../../util/number';
+import { getBlockNodeRange } from '../selection';
 import { AbstractDocumentUpdate, Command } from './type';
 import { findCutBefore } from './util';
 
@@ -63,6 +64,39 @@ export class SetNodeSelectionDocumentUpdate implements AbstractDocumentUpdate {
   public update(editorState: EditorState, tr: Transaction) {
     const { doc } = tr;
     tr.setSelection(NodeSelection.create(doc, minFromMax(this.nodePos, 0/*Doc start*/, doc.content.size)));
+    return tr/*updated*/;
+  }
+}
+
+// ................................................................................
+/** select the contents of the current parent Block Node */
+export const selectBlockNodeContentCommand: Command = (state, dispatch) => {
+  const updatedTr =  new SelectBlockNodeContentDocumentUpdate().update(state, state.tr);
+  if(updatedTr) {
+    dispatch(updatedTr);
+    return true/*Command executed*/;
+  } /* else -- Command cannot be executed */
+
+  return false/*not executed*/;
+};
+export class SelectBlockNodeContentDocumentUpdate implements AbstractDocumentUpdate {
+  public constructor() {/*nothing additional*/ }
+
+  /*
+   * modify the given Transaction such that the contents of the current
+   * parent Block Node are selected and return it
+   */
+  public update(editorState: EditorState, tr: Transaction) {
+    const { selection } = tr;
+    const { $from, empty } = selection;
+
+    if(!empty) return false/*do not overwrite Selection*/;
+    if(!$from.parent.isTextblock) return false/*not a valid Node*/;
+    if($from.parent.textContent.length < 1) return false/*nothing to Select*/;
+
+    const { from, to } = getBlockNodeRange(tr.selection);
+    tr.setSelection(TextSelection.create(tr.doc, from, to));
+
     return tr/*updated*/;
   }
 }
