f22aa194d6715356f0c75efd55aa7ebec010480a
Alan Rodz <s061199@hotmail.com>
2022-10-13 11:46:35 -0500

    add basic scrolling to preview

--- src/notebookEditor/component/NotebookViewer.tsx
+++ src/notebookEditor/component/NotebookViewer.tsx
@@ -3,16 +3,19 @@ import { convertContentToHTML, NotebookDocumentContent } from 'common';
 import { EDITOR_CLASS_NAME, EDITOR_PREVIEW_CLASS_NAME } from 'notebookEditor/theme/theme';
 
 // ********************************************************************************
-interface Props {
-  // The content of a Notebook stringified
-  content: NotebookDocumentContent;
-}
+// == Constant ====================================================================
+export const NOTEBOOK_VIEWER_ID = 'notebookViewerId';
+
+// == Interface ====================================================================
+interface Props { content: NotebookDocumentContent; }
+
+// == Component ===================================================================
 export const NotebookViewer: React.FC<Props>= ({ content }) => {
-  // Use dangerouslySetInnerHTML to the corresponding HTML string of the Notebook.
+  // uses `dangerouslySetInnerHTML` to the corresponding HTML string of the Notebook.
   // This is a security risk, but it is the only way to render the content of a
   // Notebook at the moment.
   const htmlContent = convertContentToHTML(content);
   return (
-    <div className={`${EDITOR_CLASS_NAME} ${EDITOR_PREVIEW_CLASS_NAME}`}/*SEE: /index.css*/ dangerouslySetInnerHTML={{ __html: htmlContent ?? '' }} />
+    <div id={NOTEBOOK_VIEWER_ID} className={`${EDITOR_CLASS_NAME} ${EDITOR_PREVIEW_CLASS_NAME}`}/*SEE: /index.css*/ dangerouslySetInnerHTML={{ __html: htmlContent ?? '' }} />
   );
 };
