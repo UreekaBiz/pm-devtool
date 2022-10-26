9259b051c95f4374d499813927558af1ce5149aa
Alan Rodz <s061199@hotmail.com>
2022-10-25 22:41:34 -0500

    bring missing util

--- src/common/notebookEditor/selection.ts
+++ src/common/notebookEditor/selection.ts
@@ -19,6 +19,9 @@ export type getPosType = boolean | (() => number);
 export const isGetPos = (object: any): object is (() => number) => typeof object === 'function';
 
 /** Type guard that defines if a {@link Selection} is a {@link NodeSelection} */
+export const isTextSelection = (selection: Selection): selection is TextSelection  => selection.toJSON().type === 'text';
+
+/** Type guard that defines if a {@link Selection} is a {@link NodeSelection} */
 export const isNodeSelection = (selection: Selection): selection is NodeSelection => 'node' in selection;
 
 /** Checks whether the given {@link Selection} is of GapCursor type */
