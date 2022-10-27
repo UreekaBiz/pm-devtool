b0068ac8e950ba72e8f65dc052044db2aaef0d79
Alan Rodz <s061199@hotmail.com>
2022-10-27 10:22:45 -0500

    bring add command folder

---
+++ src/notebookEditor/command/update.ts
@@ -0,0 +1,55 @@
+import { NodeSelection, Selection, TextSelection } from 'prosemirror-state';
+import { Step } from 'prosemirror-transform';
+
+import { Editor } from 'notebookEditor/API';
+
+import { isNodeSelection, AbstractDocumentUpdate } from 'common';
+
+// ********************************************************************************
+/**
+ * apply the combined effects of the given {@link AbstractDocumentUpdate}s
+ * in a single Transaction, effectively combining the effects of the Commands that
+ * use them
+ */
+export const applyDocumentUpdates = (editor: Editor, documentUpdates: AbstractDocumentUpdate[]): boolean => {
+  const steps: Step[] = [];
+  let finalSelection: Selection = editor.view.state.selection/*default*/;
+
+  let currentState = editor.view.state/*default starting state*/;
+  for(let i=0; i<documentUpdates.length; i++) {
+    // get the Transaction resulting from applying the DocumentUpdate
+    // to the current EditorState
+    const newTr = documentUpdates[i].update(currentState, currentState.tr);
+
+    // either all DocumentUpdates are valid and applied, or none of them are applied
+    if(!newTr) {
+      return false/*at least one update was not valid (SEE: AbstractDocumentUpdate)*/;
+    } /* else -- valid update */
+
+    // save the Transaction's steps and update the current State
+    for(let i = 0; i<newTr.steps.length; i++) {
+      steps.push(newTr.steps[i]);
+    }
+    currentState = currentState.apply(newTr);
+
+    // ensure the final Transaction receives the final Selection
+    if(i === documentUpdates.length-1/*last Transaction*/) {
+      finalSelection = newTr.selection;
+    } /* else -- do not change default */
+  }
+
+  // create final, applied Transaction
+  const finalTransaction = editor.view.state.tr/*new Transaction*/;
+  for(let i=0; i<steps.length; i++) {
+    finalTransaction.step(steps[i]);
+  }
+  if(isNodeSelection(finalSelection)) {
+    finalTransaction.setSelection(NodeSelection.create(finalTransaction.doc, finalSelection.from));
+  } else {
+    finalTransaction.setSelection(TextSelection.create(finalTransaction.doc, finalSelection.anchor, finalSelection.head));
+  }
+
+  editor.view.dispatch(finalTransaction);
+  setTimeout(() => editor.view.focus()/*right after changes*/);
+  return true/*updates applied*/;
+};
---
+++ src/notebookEditor/command/util.ts
@@ -0,0 +1,55 @@
+import { EditorView } from 'prosemirror-view';
+
+import { Command, SelectionDepth } from 'common';
+
+import { Editor } from 'notebookEditor/API';
+
+// ********************************************************************************
+// == Wrapper =====================================================================
+/**
+ * Executes the given {@link Command} with the current {@link Editor} props.
+ * Used by Keyboard Shortcuts
+ */
+export const shortcutCommandWrapper = (editor: Editor, command: Command) => {
+  const { state, view, dispatch } = getCommandPropsFromEditor(editor);
+  return focusViewAndReturn(command(state, dispatch), view);
+};
+
+/**
+ * Executes the given {@link Command} with the current {@link Editor} props.
+ * Used by ToolItems
+ */
+ export const toolItemCommandWrapper = (editor: Editor, depth: SelectionDepth, command: Command) => {
+  const { state, view, dispatch } = getCommandPropsFromEditor(editor);
+  return focusViewAndReturn(command(state, dispatch), view);
+};
+
+/** Returns the required props to execute a {@link Command} from a given {@link Editor} */
+const getCommandPropsFromEditor = (editor: Editor) => ({
+  state: editor.view.state,
+  view: editor.view,
+  dispatch: editor.view.dispatch,
+});
+
+/** Focuses the {@link EditorView} and returns the result from the executed {@link Command} */
+const focusViewAndReturn = (commandResult: boolean, view: EditorView) => {
+  if(commandResult) {
+    setTimeout(() => view.focus()/*immediately after the View updates*/);
+  } /* else -- Command was not executed, do not focus the View */
+
+  return commandResult;
+};
+
+// == Apply =======================================================================
+/**
+ * apply the first Command whose effects are valid from the given array of
+ * Commands. These Commands may be ProseMirror Commands or custom Commands
+ */
+export const applyFirstValidCommand = (editor: Editor, commands: Command[]): boolean => {
+  for(let i = 0; i < commands.length; i++) {
+    if(commands[i](editor.view.state, editor.view.dispatch, editor.view)) {
+      return true/*Command applied*/;
+    }
+  }
+  return false/*no Command was valid*/;
+};
