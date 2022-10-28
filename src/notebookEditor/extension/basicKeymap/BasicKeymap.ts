82ccaa364891062d7045af378e860abdddb35359
Alan Rodz <s061199@hotmail.com>
2022-10-28 09:37:53 -0500

    add basic keymap

--- src/notebookEditor/extension/basicKeymap/BasicKeymap.ts
+++ src/notebookEditor/extension/basicKeymap/BasicKeymap.ts
@@ -1,7 +1,26 @@
-keymap({
-  'Enter': chainCommands(liftEmptyBlockNodeCommand, splitBlockCommand),
-  'Backspace': chainCommands(deleteSelectionCommand, joinBackwardCommand, selectNodeBackwardCommand),
-  'Mod-Backspace': chainCommands(deleteSelectionCommand, joinBackwardCommand, selectNodeBackwardCommand),
-  'Delete': chainCommands(deleteSelectionCommand, joinForwardCommand, selectNodeForwardCommand),
-  'Mod-Delete': chainCommands(deleteSelectionCommand, joinForwardCommand, selectNodeForwardCommand),
-}),
+import { keymap } from 'prosemirror-keymap';
+
+import { chainCommands, deleteSelectionCommand, joinBackwardCommand, joinForwardCommand, liftEmptyBlockNodeCommand, selectNodeBackwardCommand, selectNodeForwardCommand, splitBlockCommand } from 'common';
+
+import { ExtensionName } from 'notebookEditor/model/type';
+
+import { DEFAULT_EXTENSION_PRIORITY, Extension } from '../Extension';
+
+// ********************************************************************************
+// == Node ========================================================================
+export const BasicKeymap = new Extension({
+  // -- Definition ----------------------------------------------------------------
+  name: ExtensionName.BASIC_KEYMAP,
+  priority: DEFAULT_EXTENSION_PRIORITY,
+
+  // -- Plugin --------------------------------------------------------------------
+  addProseMirrorPlugins: (editor) => [
+    keymap({
+      'Enter': chainCommands(liftEmptyBlockNodeCommand, splitBlockCommand),
+      'Backspace': chainCommands(deleteSelectionCommand, joinBackwardCommand, selectNodeBackwardCommand),
+      'Mod-Backspace': chainCommands(deleteSelectionCommand, joinBackwardCommand, selectNodeBackwardCommand),
+      'Delete': chainCommands(deleteSelectionCommand, joinForwardCommand, selectNodeForwardCommand),
+      'Mod-Delete': chainCommands(deleteSelectionCommand, joinForwardCommand, selectNodeForwardCommand),
+    }),
+  ],
+});
