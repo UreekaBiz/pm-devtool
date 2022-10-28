adf2b9bf440b8b98a30a74a3bde1ba873ddae43a
Alan Rodz <s061199@hotmail.com>
2022-10-28 09:41:19 -0500

    document

--- src/notebookEditor/extension/Extension.ts
+++ src/notebookEditor/extension/Extension.ts
@@ -9,13 +9,19 @@ export const DEFAULT_EXTENSION_PRIORITY = 100;
 // == Interface ===================================================================
 export interface ExtensionProps {
   readonly name: string;
+
+  /** the ProseMirror Plugins added by this Extension */
   readonly addProseMirrorPlugins: (editor: Editor) => ProseMirrorPlugin[];
+
+  /**
+   * defines the order in which the Extension's Plugins get executed
+   * a higher priority means the Plugins of the Extension get executed
+   * those of Extensions with lower priority
+   */
   readonly priority: number;
 }
 
 // == Class =======================================================================
 export class Extension {
-  constructor(public readonly props: ExtensionProps) {
-    // nothing additional
-  }
+  constructor(public readonly props: ExtensionProps) {/*nothing additional*/}
 }
