25af4863dace2697e5f459699cfa00e701deac34
Alan Rodz <s061199@hotmail.com>
2022-10-29 09:21:15 -0500

    memoize toolbar

--- src/notebookEditor/toolbar/component/Toolbar/Toolbar.tsx
+++ src/notebookEditor/toolbar/component/Toolbar/Toolbar.tsx
@@ -1,4 +1,5 @@
 import { Box, Divider, Flex, Text, VStack } from '@chakra-ui/react';
+import { useMemo } from 'react';
 
 import { MarkName, NodeName, SelectionDepth } from 'common';
 
@@ -20,7 +21,7 @@ interface Props {
 }
 
 // == Component ===================================================================
-export const Toolbar: React.FC<Props> = ({ depth, nodeOrMarkName, toolbar,  selectedDepth, onSelection }) => {
+export const Toolbar: React.FC<Props> = ({ depth, nodeOrMarkName, toolbar, selectedDepth, onSelection }) => {
   const editor = useValidatedEditor();
 
   // NOTE: A react component is meant to be rendered using JSX instead of calling
@@ -29,43 +30,45 @@ export const Toolbar: React.FC<Props> = ({ depth, nodeOrMarkName, toolbar,  sele
   //       Hooks". To avoid this a valid component is created from the same function.
   const RightContent = toolbar.rightContent;
 
-  // NOTE: The check must be done by the caller, this is just a safety check in in
-  //       case that the caller does not check it.
-  if(toolbar.shouldShow && !toolbar.shouldShow(editor, depth)) return null/*nothing to render*/;
-
   // -- UI ------------------------------------------------------------------------
-  return (
-    <Box>
-      <Flex
-        align='center'
-        justify='space-between'
-        width='full'
-        paddingX={4}
-        paddingY={2}
-        backgroundColor={depth === selectedDepth ? '#ddd' : '#f3f3f3'}
-        boxShadow='base'
-        onClick={() => onSelection(depth)}
-      >
-        <Text
-          marginBottom='1px'
-          paddingY={0.1}
-          fontSize={15}
-          fontWeight='bold'
-          textTransform='capitalize'
-          _hover={{ cursor: 'pointer' }}
+  return useMemo(() => {
+    // NOTE: The check must be done by the caller, this is just a safety check in in
+    //       case that the caller does not check it.
+    if(toolbar.shouldShow && !toolbar.shouldShow(editor, depth)) return null/*nothing to render*/;
+
+    return (
+      <Box>
+        <Flex
+          align='center'
+          justify='space-between'
+          width='full'
+          paddingX={4}
+          paddingY={2}
+          backgroundColor={depth === selectedDepth ? '#ddd' : '#f3f3f3'}
+          boxShadow='base'
+          onClick={() => onSelection(depth)}
         >
-          {toolbar.title}
-        </Text>
-        {RightContent && <RightContent depth={depth} editor={editor} />}
-      </Flex>
-      <VStack divider={<Divider />} spacing={0} display='flex' alignItems='flex-start' width='full'>
-        {toolbar.toolsCollections.map((tools, i) =>
-          <Box key={`${nodeOrMarkName}-${i}`} paddingX={4} paddingY={1} width='100%'>
-            {tools.map(tool =>
-              <ToolItemComponent key={`${nodeOrMarkName}-${tool.name}-${i}`} depth={depth} editor={editor} tool={tool} />)}
-          </Box>
+          <Text
+            marginBottom='1px'
+            paddingY={0.1}
+            fontSize={15}
+            fontWeight='bold'
+            textTransform='capitalize'
+            _hover={{ cursor: 'pointer' }}
+          >
+            {toolbar.title}
+          </Text>
+          {RightContent && <RightContent depth={depth} editor={editor} />}
+        </Flex>
+        <VStack divider={<Divider />} spacing={0} display='flex' alignItems='flex-start' width='full'>
+          {toolbar.toolsCollections.map((tools, i) =>
+            <Box key={`${nodeOrMarkName}-${i}`} paddingX={4} paddingY={1} width='100%'>
+              {tools.map(tool =>
+                <ToolItemComponent key={`${nodeOrMarkName}-${tool.name}-${i}`} depth={depth} editor={editor} tool={tool} />)}
+            </Box>
           )}
-      </VStack>
-    </Box>
-  );
+        </VStack>
+      </Box>
+    );
+  }, [RightContent, depth, editor, nodeOrMarkName, onSelection, selectedDepth, toolbar]);
 };
