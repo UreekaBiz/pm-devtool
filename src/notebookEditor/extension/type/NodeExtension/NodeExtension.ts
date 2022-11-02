11d2ff75691c64d9a3b93c71eab128abfc7f503a
Alan Rodz <s061199@hotmail.com>
2022-11-02 17:40:16 -0600

    match framework-full

--- src/notebookEditor/extension/type/NodeExtension/NodeExtension.ts
+++ src/notebookEditor/extension/type/NodeExtension/NodeExtension.ts
@@ -5,11 +5,13 @@ import { NodeExtensionDefinition, NodeExtensionNodeSpec } from './type';
 // == Class =======================================================================
 export class NodeExtension extends Extension {
   // -- Attribute -----------------------------------------------------------------
+  public definition: NodeExtensionDefinition;
   public nodeSpec: NodeExtensionNodeSpec;
 
   // -- Lifecycle -----------------------------------------------------------------
   constructor(definition: NodeExtensionDefinition) {
     super(definition);
+    this.definition = definition;
 
     const nodeAttributeDefinition = definition.defineNodeAttributes(this.storage);
     const nodeDOMBehavior = definition.defineDOMBehavior(this.storage);
