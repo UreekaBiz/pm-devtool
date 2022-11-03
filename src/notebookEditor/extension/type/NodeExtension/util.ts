60c0f316c886dfe0a6473736ada3b4ec198c4835
Alan Rodz <s061199@hotmail.com>
2022-11-03 13:23:58 -0600

    match framework full

--- src/notebookEditor/extension/type/NodeExtension/util.ts
+++ src/notebookEditor/extension/type/NodeExtension/util.ts
@@ -1,9 +1,30 @@
-import { NodeSpec } from 'prosemirror-model';
+import { Node as ProsemirrorNode, NodeSpec } from 'prosemirror-model';
+
+import { AttributeType, NodeName } from 'common';
+
+import { Editor } from 'notebookEditor/editor';
+import { isNodeViewStorage } from 'notebookEditor/model';
 
 import { Extension } from '../Extension';
 import { NodeExtension } from './NodeExtension';
 
 // == NodeExtension ===============================================================
+// -- Schema ----------------------------------------------------------------------
+/**
+ * returns the name of the {@link NodeSpec} that contains the 'topNode'
+ * property, which indicates that it is the root of the Document
+ */
+ export const getTopNode = (extensions: Extension[]) => {
+  const topNodeName = extensions.find(extension => isNodeExtension(extension) && extension.nodeSpec.topNode)?.name;
+  if(!topNodeName) throw new Error('Cannot create a Document without a topNode');
+
+  return topNodeName;
+};
+
+/** check whether the given {@link Extension} is a {@link NodeExtension} */
+export const isNodeExtension = (extension: Extension): extension is NodeExtension => 'nodeSpec' in extension;
+
+// -- Spec ------------------------------------------------------------------------
 /**
  * return an array containing only {@link NodeExtension}s given an array of
  * {@link Extension}s
@@ -15,16 +36,28 @@ import { NodeExtension } from './NodeExtension';
   return nodeExtensions;
 }, {/*default empty*/});
 
-/**
- * returns the name of the {@link NodeSpec} that contains the 'topNode'
- * property, which indicates that it is the root of the Document
- */
-export const getTopNode = (extensions: Extension[]) => {
-  const topNodeName = extensions.find(extension => isNodeExtension(extension) && extension.nodeSpec.topNode)?.name;
-  if(!topNodeName) throw new Error('Cannot create a Document without a topNode');
+// -- View ------------------------------------------------------------------------
+// REF: https://stackoverflow.com/questions/41017287/cannot-use-new-with-expression-typescript
+// allow type of controllers that extend AbstractNodeController to be used
+interface ConstructableController<ControllerType> {
+  new(...args: any/*do not restrict Controller requirements*/) : ControllerType;
+}
+export const defineNodeViewBehavior = <Controller>(editor: Editor, receivedNode: ProsemirrorNode, desiredNodeName: NodeName, getPos: () => number, nodeValidationFunction: (node: ProsemirrorNode) => boolean, NodeController: ConstructableController<Controller>) => {
+  if(!nodeValidationFunction(receivedNode)) throw new Error(`Unexpected Node: (${receivedNode.type.name}) while adding ${desiredNodeName} NodeView.`);
 
-  return topNodeName;
+  const id = receivedNode.attrs[AttributeType.Id];
+  if(!id) throw new Error(`${desiredNodeName} does not have an Id when it should by contract.`);
+
+  const storage = editor.storage.get(desiredNodeName);
+  if(!storage || !(isNodeViewStorage(storage))) throw new Error(`${desiredNodeName} does not have a valid storage when it should by contract.`);
+
+  // use existing NodeView, update it and return it
+  const controller = storage.getNodeView(id);
+  if(controller) {
+    controller.updateProps(getPos);
+    return controller;
+  } /* else -- controller don't exists */
+
+  // create a new Controller and NodeView
+  return new NodeController(editor, receivedNode, storage, getPos);
 };
-
-/** check whether the given {@link Extension} is a {@link NodeExtension} */
-export const isNodeExtension = (extension: Extension): extension is NodeExtension => 'nodeSpec' in extension;
