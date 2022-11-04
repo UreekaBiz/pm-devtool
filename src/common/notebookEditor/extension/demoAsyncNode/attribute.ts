49a256c8a30e834453f85db080c22ac0da2a556c
Alan Rodz <s061199@hotmail.com>
2022-11-04 11:47:18 -0600

    change attrs

--- src/common/notebookEditor/extension/demoAsyncNode/attribute.ts
+++ src/common/notebookEditor/extension/demoAsyncNode/attribute.ts
@@ -1,21 +1,39 @@
-import { AttributesTypeFromNodeSpecAttributes, AttributeType, noNodeOrMarkSpecAttributeDefaultValue } from '../../attribute';
-import { createDefaultCodeBlockAsyncNodeAttributes, CodeBlockAsyncNodeAttributeSpec, DEFAULT_CODEBLOCK_ASYNC_NODE_STATUS } from '../codeBlockAsyncNode';
+
+import { noNodeOrMarkSpecAttributeDefaultValue, AttributesTypeFromNodeSpecAttributes, AttributeType } from '../../attribute';
+import { AsyncNodeAttributeSpec, AsyncNodeStatus, createDefaultAsyncNodeAttributes } from '../asyncNode';
+import { CodeBlockHash } from '../codeBlock';
+import { CodeBlockReference } from '../codeBlockReference';
 
 // ********************************************************************************
 // == Attribute ===================================================================
 // NOTE: must be present on the NodeSpec below
 // NOTE: This values must have matching types the ones defined in the Extension
 export const DemoAsyncNodeAttributeSpec = {
-  ...CodeBlockAsyncNodeAttributeSpec,
+  ...AsyncNodeAttributeSpec,
 
+  /** the array of nodeIdentifiers that the async node is listening to */
+  [AttributeType.CodeBlockReferences]: noNodeOrMarkSpecAttributeDefaultValue<CodeBlockReference[]>(),
+
+  /** the array of strings containing the hashes of the textContent of each corresponding CodeBlockReference*/
+  [AttributeType.CodeBlockHashes]: noNodeOrMarkSpecAttributeDefaultValue<CodeBlockHash[]>(),
+
+  /** the resulting value of the executed function */
+  [AttributeType.Text]: noNodeOrMarkSpecAttributeDefaultValue<string>(),
+
+  /** the delay for the execution of the DAN */
   [AttributeType.Delay]: noNodeOrMarkSpecAttributeDefaultValue<number>(),
 };
 export type DemoAsyncNodeAttributes = AttributesTypeFromNodeSpecAttributes<typeof DemoAsyncNodeAttributeSpec>;
 
 // == Util ========================================================================
-export const createDefaultDemoAsyncNodeAttributes = (): Partial<DemoAsyncNodeAttributes> =>
-  ({ ...createDefaultCodeBlockAsyncNodeAttributes(), [AttributeType.Delay]: DEFAULT_DEMO_ASYNC_NODE_DELAY });
+export const createDefaultDemoAsyncNodeAttributes = (): Partial<DemoAsyncNodeAttributes> => ({
+  ...createDefaultAsyncNodeAttributes(),
+  [AttributeType.CodeBlockReferences]: [/*empty*/],
+  [AttributeType.CodeBlockHashes]: [/*empty*/],
+  [AttributeType.Status]: AsyncNodeStatus.NEVER_EXECUTED,
+  [AttributeType.Delay]: DEFAULT_DEMO_ASYNC_NODE_DELAY,
+  [AttributeType.Text]: DEFAULT_DEMO_ASYNC_NODE_TEXT,
+});
 
- export const DEFAULT_DEMO_ASYNC_NODE_STATUS = DEFAULT_CODEBLOCK_ASYNC_NODE_STATUS/*alias*/;
 export const DEFAULT_DEMO_ASYNC_NODE_TEXT = 'Not Executed'/*creation default*/;
 export const DEFAULT_DEMO_ASYNC_NODE_DELAY = 4000/*ms*/;
