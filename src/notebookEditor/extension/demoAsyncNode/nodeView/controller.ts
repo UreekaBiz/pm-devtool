858cea53b3aad483cae078305b5d55137e5a9864
Alan Rodz <s061199@hotmail.com>
2022-11-04 15:29:56 -0600

    add ignore mutation

--- src/notebookEditor/extension/demoAsyncNode/nodeView/controller.ts
+++ src/notebookEditor/extension/demoAsyncNode/nodeView/controller.ts
@@ -17,4 +17,16 @@ export class DemoAsyncNodeController extends AbstractAsyncNodeController<string,
 
     super(model, view, editor, node, storage, getPos);
   }
+
+  // .. Mutation ..................................................................
+  /**
+   * Ignore Mutations that modify the ChildList, Attributes or CharacterData
+   * of this NodeView. This happens when explicitly modifying HTML of the view.
+   * Returning true prevents the Selection from being re-read around the Mutation.
+   * @see NodeView#ignoreMutation()
+   */
+   public ignoreMutation(mutation: MutationRecord | { type: 'selection'; target: Element; }) {
+    // ignore if modifying the ChildList, Attributes or CharacterData the NodeView
+    return (mutation.type === 'childList') || (mutation.type === 'attributes') || (mutation.type === 'characterData');
+  }
 }
