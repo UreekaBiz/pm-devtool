c0cf3358543db8c112f9c2f6bd77b94e7b73df2b
Alan Rodz <s061199@hotmail.com>
2022-11-10 18:46:53 -0600

    use right names

--- src/notebookEditor/extension/table/node/table/util.ts
+++ src/notebookEditor/extension/table/node/table/util.ts
@@ -1,6 +1,6 @@
 import { Fragment, Node as ProseMirrorNode } from 'prosemirror-model';
 
-import { getTableNodeTypes, AttributeType, NotebookSchemaType, TableNodeType } from 'common';
+import { getTableNodeTypes, AttributeType, NotebookSchemaType, TableNodeType, NodeName } from 'common';
 
 import { isValidHTMLElement } from 'notebookEditor/extension/util';
 
@@ -14,12 +14,11 @@ export const createTable = (schema: NotebookSchemaType, rowsCount: number, colsC
   const cells = [];
 
   for(let index = 0; index < colsCount; index += 1) {
-    const cellType = types.cell,
-          headerType = types.header_cell;
+    const cellType = types[NodeName.CELL];
+    const headerType = types[NodeName.HEADER_CELL];
     if(!cellType || !headerType) throw new Error('Cell or Header type not defined defined. Check that the correct names are being used');
 
     const cell = createCell(cellType, cellContent);
-
     if(!cell) throw new Error('Cell not created successfully when it was meant to.');
     cells.push(cell);
 
@@ -28,16 +27,15 @@ export const createTable = (schema: NotebookSchemaType, rowsCount: number, colsC
       if(!headerCell) throw new Error('Header cell not created successfully when it was meant to.');
 
       headerCells.push(headerCell);
-    }
-    /* else -- do not add header row */
+    } /* else -- do not add header row */
   }
 
   const rows = [];
   for(let index = 0; index < rowsCount; index += 1) {
-    rows.push(types.row.createChecked(null, withHeaderRow && index === 0 ? headerCells : cells));
+    rows.push(types.row.createChecked(null/*no attrs*/, withHeaderRow && index === 0 ? headerCells : cells));
   }
 
-  return types.table.createChecked(null, rows);
+  return types.table.createChecked(null/*no attrs*/, rows);
 };
 
 // == Update ======================================================================
