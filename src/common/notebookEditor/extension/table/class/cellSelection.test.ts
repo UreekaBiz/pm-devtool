1adbf495c57678d3fd8f01daa0776cc544d0e773
Alan Rodz <s061199@hotmail.com>
2022-11-12 01:04:09 -0600

    add tests

--- src/common/notebookEditor/extension/table/class/cellSelection.test.ts
+++ src/common/notebookEditor/extension/table/class/cellSelection.test.ts
@@ -1,11 +1,11 @@
 import ist from 'ist';
-// import { Slice } from 'prosemirror-model';
+import { Node as ProseMirrorNode, Slice } from 'prosemirror-model';
 import { Command, EditorState } from 'prosemirror-state';
-// import { eq } from 'prosemirror-test-builder';
 
-import { addRowBeforeCommand } from '../../../../notebookEditor/command/table';
-import { getNotebookSchemaNodeBuilders } from '../../../../notebookEditor/command/test/testUtil';
-import { emptyCellBuilder } from '../../../../notebookEditor/command/test/tableTestUtil';
+import { AttributeType } from '../../../../notebookEditor/attribute';
+import { addColumnAfterCommand, addColumnBeforeCommand, addRowAfterCommand, addRowBeforeCommand } from '../../../../notebookEditor/command/table';
+import { cellBuilder, cellWithAnchorBuilder, cellWithDimensionBuilder, cellWithHeadBuilder, defaultCellBuilder, defaultRowBuilder, defaultTableBuilder, emptyCellBuilder, selectionForTableTest } from '../../../../notebookEditor/command/test/tableTestUtil';
+import { getNotebookSchemaNodeBuilders, A } from '../../../../notebookEditor/command/test/testUtil';
 import { NodeName } from '../../../../notebookEditor/node';
 import { isCellSelection } from '../../../../notebookEditor/selection';
 
@@ -13,21 +13,17 @@ import { CellSelection } from './CellSelection';
 
 // ********************************************************************************
 // == Constant ====================================================================
-const {
-  [NodeName.DOC]: docBuilder,
-  [NodeName.ROW]: rowBuilder,
-  [NodeName.TABLE]: tableBuilder,
-} = getNotebookSchemaNodeBuilders([NodeName.DOC, NodeName.ROW, NodeName.TABLE]);
+const { [NodeName.DOC]: docBuilder, [NodeName.PARAGRAPH]: paragraphBuilder } = getNotebookSchemaNodeBuilders([NodeName.DOC, NodeName.PARAGRAPH]);
 
 // == Test ========================================================================
 describe('CellSelection', () => {
   // NOTE: the inline comments below are the positions of the
   // .     start of the Cell Nodes
   const tableDoc = docBuilder(
-    tableBuilder(
-      rowBuilder(/* 2*/ emptyCellBuilder, /* 6*/ emptyCellBuilder, /*10*/ emptyCellBuilder),
-      rowBuilder(/*16*/ emptyCellBuilder, /*20*/ emptyCellBuilder, /*24*/ emptyCellBuilder),
-      rowBuilder(/*30*/ emptyCellBuilder, /*34*/ emptyCellBuilder, /*36*/ emptyCellBuilder)
+    defaultTableBuilder(
+        defaultRowBuilder(/* 2*/ emptyCellBuilder, /* 6*/ emptyCellBuilder, /*10*/ emptyCellBuilder),
+        defaultRowBuilder(/*16*/ emptyCellBuilder, /*20*/ emptyCellBuilder, /*24*/ emptyCellBuilder),
+        defaultRowBuilder(/*30*/ emptyCellBuilder, /*34*/ emptyCellBuilder, /*36*/ emptyCellBuilder)
     )
   );
 
@@ -61,133 +57,111 @@ describe('CellSelection', () => {
     ist(selection.$anchorCell.pos, 48);
     ist(selection.$headCell.pos, 6);
 
-    // selection = executeCellSelectionTestCommand(6, 30, addRowAfterCommand).selection;
-    // if(!isCellSelection(selection)) throw new Error('expected a CellSelection');
-    // ist(selection.$anchorCell.pos, 6);
-    // ist(selection.$headCell.pos, 44);
+    selection = executeCellSelectionTestCommand(6, 30, addRowAfterCommand).selection;
+    if(!isCellSelection(selection)) throw new Error('expected a CellSelection');
+    ist(selection.$anchorCell.pos, 6);
+    ist(selection.$headCell.pos, 44);
   });
 
-//   it('extends a col selection when adding a column', () => {
-//     let sel = executeCommand(16, 24, addColumnAfter).selection;
-//     ist(sel.$anchorCell.pos, 20);
-//     ist(sel.$headCell.pos, 32);
-//     sel = executeCommand(24, 30, addColumnBefore).selection;
-//     ist(sel.$anchorCell.pos, 32);
-//     ist(sel.$headCell.pos, 38);
-//   });
-// });
+  it('extends a col selection when adding a column', () => {
+    let selection = executeCellSelectionTestCommand(16, 24, addColumnAfterCommand).selection;
+    if(!isCellSelection(selection)) throw new Error('expected a CellSelection');
+    ist(selection.$anchorCell.pos, 20);
+    ist(selection.$headCell.pos, 32);
 
-// describe('CellSelection.content', () => {
-//   function slice(doc) {
-//     return new Slice(doc.content, 1, 1);
-//   }
-
-//   it('contains only the selected cells', () =>
-//     ist(
-//       selectionForTableTest(
-//         tableBuilder(
-//           rowBuilder(c11, cAnchor, emptyCellBuilder),
-//           rowBuilder(c11, emptyCellBuilder, cHead),
-//           rowBuilder(c11, c11, c11)
-//         )
-//       ).content(),
-//       slice(tableBuilder('<a>', rowBuilder(c11, emptyCellBuilder), rowBuilder(emptyCellBuilder, c11))),
-//       eq
-//     ));
-
-//   it('understands spanning cells', () =>
-//     ist(
-//       selectionForTableTest(
-//         tableBuilder(rowBuilder(cAnchor, c(2, 2), c11, c11), rowBuilder(c11, cHead, c11, c11))
-//       ).content(),
-//       slice(tableBuilder(rowBuilder(c11, c(2, 2), c11), rowBuilder(c11, c11))),
-//       eq
-//     ));
-
-//   it('cuts off cells sticking out horizontally', () =>
-//     ist(
-//       selectionForTableTest(
-//         tableBuilder(rowBuilder(c11, cAnchor, c(2, 1)), rowBuilder(c(4, 1)), rowBuilder(c(2, 1), cHead, c11))
-//       ).content(),
-//       slice(tableBuilder(rowBuilder(c11, c11), rowBuilder(td({ colspan: 2 }, p())), rowBuilder(emptyCellBuilder, c11))),
-//       eq
-//     ));
-
-//   it('cuts off cells sticking out vertically', () =>
-//     ist(
-//       selectionForTableTest(
-//         tableBuilder(
-//           rowBuilder(c11, c(1, 4), c(1, 2)),
-//           rowBuilder(cAnchor),
-//           rowBuilder(c(1, 2), cHead),
-//           rowBuilder(c11)
-//         )
-//       ).content(),
-//       slice(tableBuilder(rowBuilder(c11, td({ rowspan: 2 }, p()), emptyCellBuilder), rowBuilder(c11, c11))),
-//       eq
-//     ));
-
-//   it('preserves column widths', () =>
-//     ist(
-//       selectionForTableTest(
-//         tableBuilder(
-//           rowBuilder(c11, cAnchor, c11),
-//           rowBuilder(td({ colspan: 3, colwidth: [100, 200, 300] }, p('x'))),
-//           rowBuilder(c11, cHead, c11)
-//         )
-//       ).content(),
-//       slice(tableBuilder(rowBuilder(c11), rowBuilder(td({ colwidth: [200] }, p())), rowBuilder(c11))),
-//       eq
-//     ));
-// });
-
-// describe('normalizeSelection', () => {
-//   let t = docBuilder(
-//     tableBuilder(
-//       rowBuilder(/* 2*/ c11, /* 7*/ c11, /*12*/ c11),
-//       rowBuilder(/*19*/ c11, /*24*/ c11, /*29*/ c11),
-//       rowBuilder(/*36*/ c11, /*41*/ c11, /*46*/ c11)
-//     )
-//   );
-
-//   function normalize(selection, { allowTableNodeSelection = false } = {}) {
-//     let state = EditorState.create({
-//       doc: t,
-//       selection,
-//       plugins: [tableEditing({ allowTableNodeSelection })],
-//     });
-//     return state.apply(state.tr).selection;
-//   }
-
-//   it('converts a table node selection into a selection of all cells in the table', () => {
-//     ist(
-//       normalize(NodeSelection.create(t, 0)),
-//       CellSelection.create(t, 2, 46),
-//       eq
-//     );
-//   });
-
-//   it('retains a table node selection if the allowTableNodeSelection option is true', () => {
-//     ist(
-//       normalize(NodeSelection.create(t, 0), { allowTableNodeSelection: true }),
-//       NodeSelection.create(t, 0),
-//       eq
-//     );
-//   });
-
-//   it('converts a row node selection into a cell selection', () => {
-//     ist(
-//       normalize(NodeSelection.create(t, 1)),
-//       CellSelection.create(t, 2, 12),
-//       eq
-//     );
-//   });
-
-//   it('converts a cell node selection into a cell selection', () => {
-//     ist(
-//       normalize(NodeSelection.create(t, 2)),
-//       CellSelection.create(t, 2, 2),
-//       eq
-//     );
-//   });
+    selection = executeCellSelectionTestCommand(24, 30, addColumnBeforeCommand).selection;
+    if(!isCellSelection(selection)) throw new Error('expected a CellSelection');
+    ist(selection.$anchorCell.pos, 32);
+    ist(selection.$headCell.pos, 38);
+  });
 });
+
+describe('CellSelection.content', () => {
+  const sliceStartAndEnd = (doc: ProseMirrorNode) => new Slice(doc.content, 1/*cut one position at start*/, 1/*cut one position at end*/);
+  const compareStringifiedSlice = (a: Slice, b: Slice) => JSON.stringify(a) === JSON.stringify(b);
+
+  it('contains only the selected cells', () => {
+    const selectionContent =
+      selectionForTableTest(
+          defaultTableBuilder(
+            defaultRowBuilder(cellBuilder, cellWithAnchorBuilder, emptyCellBuilder),
+            defaultRowBuilder(cellBuilder, emptyCellBuilder, cellWithHeadBuilder),
+            defaultRowBuilder(cellBuilder, cellBuilder, cellBuilder)
+       )
+      )?.content();
+
+    ist(selectionContent, sliceStartAndEnd(
+      defaultTableBuilder(`<${A}>`, defaultRowBuilder(cellBuilder, emptyCellBuilder),
+                                    defaultRowBuilder(emptyCellBuilder, cellBuilder))),
+      compareStringifiedSlice);
+  });
+
+  it('understands spanning cells', () => {
+    const selectionContent =
+      selectionForTableTest(
+          defaultTableBuilder(
+            defaultRowBuilder(cellWithAnchorBuilder, cellWithDimensionBuilder(2, 2), cellBuilder, cellBuilder),
+            defaultRowBuilder(cellBuilder, cellWithHeadBuilder, cellBuilder, cellBuilder))
+      )?.content();
+
+    ist(selectionContent, sliceStartAndEnd(
+      defaultTableBuilder(
+        defaultRowBuilder(cellBuilder, cellWithDimensionBuilder(2, 2), cellBuilder),
+        defaultRowBuilder(cellBuilder, cellBuilder))),
+      compareStringifiedSlice);
+  });
+
+  it('cuts off cells sticking out horizontally', () => {
+    const selectionContent =
+      selectionForTableTest(
+          defaultTableBuilder(
+            defaultRowBuilder(cellBuilder, cellWithAnchorBuilder, cellWithDimensionBuilder(2, 1)),
+            defaultRowBuilder(cellWithDimensionBuilder(4, 1)),
+            defaultRowBuilder(cellWithDimensionBuilder(2, 1), cellWithHeadBuilder, cellBuilder))
+      )?.content();
+
+    ist(selectionContent, sliceStartAndEnd(
+      defaultTableBuilder(
+        defaultRowBuilder(cellBuilder, cellBuilder),
+        defaultRowBuilder(defaultCellBuilder({ [AttributeType.ColSpan]: 2 }, paragraphBuilder())),
+        defaultRowBuilder(emptyCellBuilder, cellBuilder))),
+      compareStringifiedSlice);
+  });
+
+  it('cuts off cells sticking out vertically', () => {
+    const selectionContent =
+      selectionForTableTest(
+        defaultTableBuilder(
+          defaultRowBuilder(cellBuilder, cellWithDimensionBuilder(1, 4), cellWithDimensionBuilder(1, 2)),
+          defaultRowBuilder(cellWithAnchorBuilder),
+          defaultRowBuilder(cellWithDimensionBuilder(1, 2), cellWithHeadBuilder),
+          defaultRowBuilder(cellBuilder)
+        )
+      )?.content();
+
+    ist(selectionContent,
+      sliceStartAndEnd(defaultTableBuilder(
+        defaultRowBuilder(cellBuilder, defaultCellBuilder({ [AttributeType.RowSpan]: 2 }, paragraphBuilder()), emptyCellBuilder),
+        defaultRowBuilder(cellBuilder, cellBuilder))),
+      compareStringifiedSlice);
+  });
+
+  it('preserves column widths', () => {
+    const selectionContent =
+      selectionForTableTest(
+        defaultTableBuilder(
+          defaultRowBuilder(cellBuilder, cellWithAnchorBuilder, cellBuilder),
+          defaultRowBuilder(defaultCellBuilder({ [AttributeType.ColSpan]: 3, [AttributeType.ColWidth]: [100, 200, 300] }, paragraphBuilder('x'))),
+          defaultRowBuilder(cellBuilder, cellWithHeadBuilder, cellBuilder)
+        )
+      )?.content();
+
+    ist(selectionContent,
+      sliceStartAndEnd(defaultTableBuilder(defaultRowBuilder(cellBuilder),
+        defaultRowBuilder(defaultCellBuilder({ [AttributeType.ColWidth]: [200] }, paragraphBuilder())),
+        defaultRowBuilder(cellBuilder))),
+      compareStringifiedSlice);
+  });
+
+});
+
