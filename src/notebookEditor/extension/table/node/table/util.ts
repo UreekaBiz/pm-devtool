a50f268b178a33305e0af6cae54e1b506aa89ce6
Alan Rodz <s061199@hotmail.com>
2022-11-17 14:57:52 -0600

    modify

--- src/notebookEditor/extension/table/node/table/util.ts
+++ src/notebookEditor/extension/table/node/table/util.ts
@@ -9,44 +9,44 @@ import { Unit } from 'notebookEditor/theme';
 
 /** update the Columns of a Table */
 export const updateTableColumns = (node: TableNodeType, columnGroup: HTMLTableColElement, tableDOM: HTMLTableElement, minimumCellWidth: number, overrideCol?: number, overrideValue?: number) => {
-  let totalWidth = 0/*default*/;
-  let fixedWidth = true/*default*/;
+  let totalWidth = 0/*default*/,
+      fixedWidth = true/*default*/;
 
-  let nextDOM = columnGroup.firstChild;
+  let nextDOMNode = columnGroup.firstChild/**default*/;
 
   const row = node.firstChild;
   if(!row) return/*nothing to do*/;
 
   for(let i = 0, col = 0; i < row.childCount; i++) {
-    const colSpan = row.child(i).attrs[AttributeType.ColSpan];
-    const colWidth = row.child(i).attrs[AttributeType.ColWidth];
+    const colSpan = row.child(i).attrs[AttributeType.ColSpan],
+          colWidth = row.child(i).attrs[AttributeType.ColWidth];
 
     for(let j = 0; j < colSpan; j++, col++) {
-      const hasWidth = overrideCol === col ? overrideValue : colWidth && colWidth[j];
-      const cssWidth = hasWidth ? hasWidth + Unit.Pixel : ''/*none*/;
+      const hasWidth = overrideCol === col ? overrideValue : colWidth && colWidth[j],
+            cssWidth = hasWidth ? hasWidth + Unit.Pixel : ''/*none*/;
 
       totalWidth += hasWidth || minimumCellWidth;
       if(!hasWidth) {
         fixedWidth = false;
       } /* else -- do not change default */
 
-      if(!nextDOM) {
+      if(!nextDOMNode) {
         const newColumn = document.createElement('col');
         columnGroup.appendChild(newColumn).style.width = cssWidth;
       } else {
-        if(isValidHTMLElement(nextDOM) && nextDOM.style.width !== cssWidth) {
-          nextDOM.style.width = cssWidth;
+        if(isValidHTMLElement(nextDOMNode) && nextDOMNode.style.width !== cssWidth) {
+          nextDOMNode.style.width = cssWidth;
         } /* else -- no need to change width */
 
-        nextDOM = nextDOM.nextSibling;
+        nextDOMNode = nextDOMNode.nextSibling;
       }
     }
   }
 
-  while(nextDOM) {
-    const after = nextDOM.nextSibling;
-    nextDOM.parentNode?.removeChild(nextDOM);
-    nextDOM = after;
+  while(nextDOMNode) {
+    const after = nextDOMNode.nextSibling;
+    nextDOMNode.parentNode?.removeChild(nextDOMNode);
+    nextDOMNode = after;
   }
 
   if(fixedWidth) {
