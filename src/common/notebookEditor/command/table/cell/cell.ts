ffe2a3ea8718bc6079ca8bc9419375a081c8b85a
Alan Rodz <s061199@hotmail.com>
2022-11-17 23:34:57 -0600

    add specificity

--- src/common/notebookEditor/command/table/cell/cell.ts
+++ src/common/notebookEditor/command/table/cell/cell.ts
@@ -13,59 +13,62 @@ import { AbstractDocumentUpdate } from '../../type';
 // NOTE: these are inspired by https://github.com/ProseMirror/prosemirror-tables/blob/master/src/commands.js
 
 /**
- * merge the selected Cells into a single Cell, only available
- * when the selected Cell's outline forms a Rectangle
+ * merge the selected Cells into a single Cell, if
+ * its outline forms a Rectangle
  */
 export const mergeCellsCommand: Command = (state, dispatch) =>
   AbstractDocumentUpdate.execute(new MergeCellsDocumentUpdate().update(state, state.tr), dispatch);
 export class MergeCellsDocumentUpdate implements AbstractDocumentUpdate {
   constructor() {/*nothing additional*/ }
 
+  /**
+   * modify the given Transaction such that the selected Cells are
+   * merged into a single Cell, if its outline forms a Rectangle
+   */
   public update(editorState: EditorState, tr: Transaction) {
     const { selection } = editorState;
     if(!isCellSelection(selection) || selection.$anchorCell.pos == selection.$headCell.pos) return false/*nothing to do*/;
 
-    const rect = getSelectedTableRect(editorState);
-    if(!rect) return false/*nothing to do*/;
+    const tableRect = getSelectedTableRect(editorState);
+    if(!tableRect) return false/*nothing to do*/;
 
-    const { table, tableMap, tableStart } = rect;
+    const { table, tableMap, tableStart } = tableRect;
     if(!isNotNullOrUndefined<ProseMirrorNode>(table) || !isNotNullOrUndefined<TableMap>(tableMap) || !isNotNullOrUndefined<number>(tableStart)) return false/*nothing to do*/;
-    if(cellsOverlapRectangle(tableMap.map, tableMap.width, tableMap.height, rect)) return false/*nothing to do*/;
+    if(areCellsOverlappingTableRect(tableMap.map, tableMap.width, tableMap.height, tableRect)) return false/*nothing to do*/;
 
-    const seen: { [key: number]: boolean; } = {};
-    let content = Fragment.empty;
+    const seenCellPositions: { [key: number]: boolean; } = {/*default empty*/};
+    let mergedCellContent = Fragment.empty/*default*/;
     let mergedPos: number | undefined = undefined/*default*/;
     let mergedCell: ProseMirrorNode | undefined = undefined/*default*/;
 
-    for(let row = rect.top; row < rect.bottom; row++) {
-      for(let col = rect.left; col < rect.right; col++) {
-        const cellPos = tableMap.map[row * tableMap.width + col];
-        const cell = table.nodeAt(cellPos);
+    for(let row = tableRect.top; row < tableRect.bottom; row++) {
+      for(let column = tableRect.left; column < tableRect.right; column++) {
+        const cellPos = tableMap.map[row * tableMap.width + column],
+              cellNode = table.nodeAt(cellPos);
+        if(!cellNode || seenCellPositions[cellPos]) continue/*already seen*/;
 
-        if(!cell || seen[cellPos]) continue/*already seen*/;
-
-        seen[cellPos] = true;
+        seenCellPositions[cellPos] = true/*by definition*/;
         if(mergedPos === undefined) {
           mergedPos = cellPos;
-          mergedCell = cell;
+          mergedCell = cellNode;
         } else {
-          if(!isCellEmpty(cell)) {
-            content = content.append(cell.content);
+          if(!isCellEmpty(cellNode)) {
+            mergedCellContent = mergedCellContent.append(cellNode.content);
           } /* else -- cell is not empty */
 
           const mappedCellPos = tr.mapping.map(cellPos + tableStart);
-          tr.delete(mappedCellPos, mappedCellPos + cell.nodeSize);
+          tr.delete(mappedCellPos, mappedCellPos + cellNode.nodeSize);
         }
       }
     }
 
     if(!isNotNullOrUndefined<number>(mergedPos) || !mergedCell) return false/*nothing to do*/;
 
-    tr.setNodeMarkup(mergedPos + tableStart, null/*maintain type*/, updateTableNodeAttributes(addColumnSpans(mergedCell.attrs, mergedCell.attrs[AttributeType.ColSpan], rect.right - rect.left - mergedCell.attrs[AttributeType.ColSpan]), AttributeType.RowSpan, rect.bottom - rect.top));
-    if(content.size) {
-      let end = mergedPos + 1 + mergedCell.content.size;
-      let start = isCellEmpty(mergedCell) ? mergedPos + 1 : end;
-      tr.replaceWith(start + tableStart, end + tableStart, content);
+    tr.setNodeMarkup(mergedPos + tableStart, null/*maintain type*/, updateTableNodeAttributes(addColumnSpans(mergedCell.attrs, mergedCell.attrs[AttributeType.ColSpan], tableRect.right - tableRect.left - mergedCell.attrs[AttributeType.ColSpan]), AttributeType.RowSpan, tableRect.bottom - tableRect.top));
+    if(mergedCellContent.size) {
+      let mergedCellContentEnd = mergedPos + 1 + mergedCell.content.size,
+          mergedCellContentStart = isCellEmpty(mergedCell) ? mergedPos + 1 : mergedCellContentEnd;
+      tr.replaceWith(mergedCellContentStart + tableStart, mergedCellContentEnd + tableStart, mergedCellContent);
     } /* else -- no content */
 
     tr.setSelection(new CellSelection(tr.doc.resolve(mergedPos + tableStart)));
@@ -74,9 +77,8 @@ export class MergeCellsDocumentUpdate implements AbstractDocumentUpdate {
 }
 
 /**
- * split a selected Cell whose rowSpan or colSpan is greater than one
- * into smaller Cell with the Cell type (th, td) returned by the
- * given getCellType function
+ * type of the function that returns the type of the Cell to be created
+ * whenever a Cell whose rowSpan or colSpan is greater than one is split
  */
 export type GetCellTypeFunctionType = (state: EditorState, row: number, col: number, node: ProseMirrorNode) => NodeType;
 const defaultGetCellTypeFunction: GetCellTypeFunctionType = (state, row, col, node) => {
@@ -85,8 +87,8 @@ const defaultGetCellTypeFunction: GetCellTypeFunctionType = (state, row, col, no
 };
 
 /**
- * split a selected Cell whose rowSpan or colSpan is greater than one
- * into smaller Cells. Use the first CellType for the new Cells
+ * split the selected Cell if its rowSpan or colSPan is greater than one
+ * into smaller Cells, using the type returned by the getCellTypeFunction
  */
 export const splitCellCommand = (getCellTypeFunction?: GetCellTypeFunctionType): Command => (state, dispatch) =>
   AbstractDocumentUpdate.execute(new SplitCellDocumentUpdate(getCellTypeFunction).update(state, state.tr), dispatch);
@@ -94,10 +96,14 @@ export const splitCellCommand = (getCellTypeFunction?: GetCellTypeFunctionType):
 export class SplitCellDocumentUpdate implements AbstractDocumentUpdate {
   constructor(private readonly getCellTypeFunction: GetCellTypeFunctionType = defaultGetCellTypeFunction) {/*nothing additional*/ }
 
+  /**
+   * modify the given Transaction such that the selected Cell is split such
+   * that the selected Cells are split into smaller Cells
+   */
   public update(editorState: EditorState, tr: Transaction) {
     const { selection } = editorState;
-    let cellNode: ProseMirrorNode | null = null/*default*/;
-    let cellPos: number | undefined = undefined/*default*/;
+    let cellPos: number | undefined = undefined/*default*/,
+        cellNode: ProseMirrorNode | null = null/*default*/;
 
     if(!isCellSelection(selection)) {
       cellNode = getCellWrapperAtResolvedPos(selection.$from);
@@ -115,7 +121,7 @@ export class SplitCellDocumentUpdate implements AbstractDocumentUpdate {
     if(cellNode.attrs[AttributeType.ColSpan] === 1 && cellNode.attrs[AttributeType.RowSpan] === 1) return false/*Cell is not merged*/;
 
     let baseAttrs = cellNode.attrs;
-    const attrs = [];
+    const cellsAttrsArray = [/*default empty*/];
     const colWidth = baseAttrs[AttributeType.ColWidth];
 
     if(baseAttrs[AttributeType.RowSpan] > 1) {
@@ -126,41 +132,41 @@ export class SplitCellDocumentUpdate implements AbstractDocumentUpdate {
       baseAttrs = updateTableNodeAttributes(baseAttrs, AttributeType.ColSpan, 1);
     } /* else -- no need to change colSpan */
 
-    const rect = getSelectedTableRect(editorState);
-    if(!rect) return false/*no selected Rectangle in Table*/;
+    const tableRect = getSelectedTableRect(editorState);
+    if(!tableRect) return false/*no selected Rectangle in Table*/;
 
-    const { table, tableMap, tableStart } = rect;
+    const { table, tableMap, tableStart } = tableRect;
     if(!isNotNullOrUndefined<ProseMirrorNode>(table) || !isNotNullOrUndefined<TableMap>(tableMap) || !isNotNullOrUndefined<number>(tableStart)) return false/*nothing to do*/;
 
-    for(let i = 0; i < rect.right - rect.left; i++) {
-      attrs.push(colWidth ? updateTableNodeAttributes(baseAttrs, AttributeType.ColWidth, colWidth && colWidth[i] ? [colWidth[i]] : null) : baseAttrs);
+    for(let i = 0; i < tableRect.right - tableRect.left; i++) {
+      cellsAttrsArray.push(colWidth ? updateTableNodeAttributes(baseAttrs, AttributeType.ColWidth, colWidth && colWidth[i] ? [colWidth[i]] : null/*no columnWidth specified*/) : baseAttrs);
     }
 
-    let lastCell;
-    for(let row = rect.top; row < rect.bottom; row++) {
-      let pos = tableMap.cellPositionAt(row, rect.left, table);
-      if(row === rect.top) {
-        pos += cellNode.nodeSize;
+    let lastCellPos: number | undefined = undefined;
+    for(let row = tableRect.top; row < tableRect.bottom; row++) {
+      let cellPos = tableMap.cellPositionAt(row, tableRect.left, table);
+      if(row === tableRect.top) {
+        cellPos += cellNode.nodeSize;
       } /* else -- no need to change row */
 
-      for(let col = rect.left, i = 0; col < rect.right; col++, i++) {
-        if(col === rect.left && row === rect.top) continue/* no need to insert content*/;
+      for(let column = tableRect.left, i = 0; column < tableRect.right; column++, i++) {
+        if(column === tableRect.left && row === tableRect.top) continue/* no need to insert content*/;
 
-        const cellType = this.getCellTypeFunction(editorState, row, col, cellNode);
+        const cellType = this.getCellTypeFunction(editorState, row, column, cellNode);
         if(!cellType) continue/*no CellType available*/;
 
-        const newCell = cellType.createAndFill(attrs[i]);
+        const newCell = cellType.createAndFill(cellsAttrsArray[i]);
         if(!newCell) continue/*could not create new Cell*/;
 
-        tr.insert((lastCell = tr.mapping.map(pos + tableStart, 1)), newCell);
+        tr.insert((lastCellPos = tr.mapping.map(cellPos + tableStart, 1/*associate to the right*/)), newCell);
       }
     }
 
     if(!isNotNullOrUndefined<number>(cellPos)) return false/*no position to change Cell*/;
-    tr.setNodeMarkup(cellPos, this.getCellTypeFunction(editorState, rect.top, rect.left, cellNode), attrs[0]);
+    tr.setNodeMarkup(cellPos, this.getCellTypeFunction(editorState, tableRect.top, tableRect.left, cellNode), cellsAttrsArray[0/*first attrs object*/]);
 
-    if(lastCell && isCellSelection(selection)) {
-      const $lastCellPos = tr.doc.resolve(lastCell);
+    if(lastCellPos && isCellSelection(selection)) {
+      const $lastCellPos = tr.doc.resolve(lastCellPos);
       tr.setSelection(new CellSelection(tr.doc.resolve(selection.$anchorCell.pos), $lastCellPos));
     } /* else -- no need to set CellSelection */
 
@@ -177,49 +183,51 @@ export class GoToCellDocumentUpdate implements AbstractDocumentUpdate {
   public update(editorState: EditorState, tr: Transaction) {
     if(!isSelectionHeadInRow(editorState)) return false/*nothing to do*/;
 
-    let $cell = getResolvedCellPos(editorState);
-    if(!$cell) return false/*nothing to do*/;
+    let resolvedCellPos = getResolvedCellPos(editorState);
+    if(!resolvedCellPos) return false/*nothing to do*/;
 
-    const cellPos = findCell($cell, this.direction);
+    const cellPos = findCellAtDirection(resolvedCellPos, this.direction);
     if(!cellPos) return false/*nothing to do*/;
 
-    $cell = editorState.doc.resolve(cellPos);
-    const $movedForwardCell = moveResolvedCellPosForward($cell);
+    resolvedCellPos = editorState.doc.resolve(cellPos);
+    const $movedForwardCell = moveResolvedCellPosForward(resolvedCellPos);
     if(!$movedForwardCell) return false/*could not move cell forward*/;
 
-    tr.setSelection(TextSelection.between($cell, $movedForwardCell)).scrollIntoView();
+    tr.setSelection(TextSelection.between(resolvedCellPos, $movedForwardCell)).scrollIntoView();
     return tr/*updated*/;
   }
 }
 // == Util ========================================================================
+/* check if the given CellNode is empty */
 const isCellEmpty = (cell: ProseMirrorNode) => {
   const { content: cellContent } = cell;
 
-  return (cellContent.childCount === 1
+  return (cellContent.childCount === 1/*Cell has only a single child*/
     && cellContent.firstChild
     && cellContent.firstChild.isTextblock
-    && cellContent.firstChild.childCount === 0
+    && cellContent.firstChild.childCount === 0/*textBlock has no children*/
   );
 };
 
-const cellsOverlapRectangle = (map: number[], width: number, height: number, rect: TableRect) => {
-  let indexTop = rect.top * width + rect.left;
-  let indexLeft = indexTop;
+/* check if the Cells of a TableMap overlap their TableRect */
+const areCellsOverlappingTableRect = (tableMap: number[], width: number, height: number, tableRect: TableRect) => {
+  let indexTop = tableRect.top * width + tableRect.left,
+      indexLeft = indexTop;
 
-  let indexBottom = (rect.bottom - 1) * width + rect.left;
-  let indexRight = indexTop + (rect.right - rect.left - 1);
+  let indexBottom = (tableRect.bottom - 1) * width + tableRect.left,
+      indexRight = indexTop + (tableRect.right - tableRect.left - 1);
 
-  for(let i = rect.top; i < rect.bottom; i++) {
-    if((rect.left > 0 && map[indexLeft] === map[indexLeft - 1]) || (rect.right < width && map[indexRight] === map[indexRight + 1])) {
-      return true;
+  for(let i = tableRect.top; i < tableRect.bottom; i++) {
+    if((tableRect.left > 0 && tableMap[indexLeft] === tableMap[indexLeft - 1]) || (tableRect.right < width && tableMap[indexRight] === tableMap[indexRight + 1])) {
+      return true/*overlap*/;
     } /* else -- keep looking */
     indexLeft += width;
     indexRight += width;
   }
 
-  for(let i = rect.left; i < rect.right; i++) {
-    if((rect.top > 0 && map[indexTop] === map[indexTop - width]) || (rect.bottom < height && map[indexBottom] === map[indexBottom + width])) {
-      return true;
+  for(let i = tableRect.left; i < tableRect.right; i++) {
+    if((tableRect.top > 0 && tableMap[indexTop] === tableMap[indexTop - width]) || (tableRect.bottom < height && tableMap[indexBottom] === tableMap[indexBottom + width])) {
+      return true/*overlap*/;
     } /* else -- keep looking */
 
     indexTop++;
@@ -230,15 +238,15 @@ const cellsOverlapRectangle = (map: number[], width: number, height: number, rec
   return false/*default*/;
 };
 
-const findCell = ($cell: ResolvedPos, dir: 'previous' | 'next') => {
-  if(dir === 'previous') {
+const findCellAtDirection = ($cell: ResolvedPos, direction: 'previous' | 'next') => {
+  if(direction === 'previous') {
     const { nodeBefore } = $cell;
     if(nodeBefore) {
       return $cell.pos - nodeBefore.nodeSize;
     } /* else -- no nodeBefore */
 
-    for(let row = $cell.index(-1) - 1, rowEnd = $cell.before(); row >= 0; row--) {
-      const rowNode = $cell.node(-1).child(row);
+    for(let row = $cell.index(-1/*grandParent*/) - 1, rowEnd = $cell.before(); row >= 0; row--) {
+      const rowNode = $cell.node(-1/*grandParent*/).child(row);
       if(rowNode.childCount && rowNode.lastChild) {
         return rowEnd - 1 - rowNode.lastChild.nodeSize;
       } /* else -- rowNode has no children or no lastChild */
@@ -251,9 +259,9 @@ const findCell = ($cell: ResolvedPos, dir: 'previous' | 'next') => {
       return $cell.pos + $cell.nodeAfter.nodeSize;
     } /* else -- $cell index is less than the childCount of $cell's parent, or $cell has no nodeAfter */
 
-    const table = $cell.node(-1);
-    for(let row = $cell.indexAfter(-1), rowStart = $cell.after(); row < table.childCount; row++) {
-      const rowNode = table.child(row);
+    const tableNode = $cell.node(-1/*grandParent*/);
+    for(let row = $cell.indexAfter(-1/*grandParent depth*/), rowStart = $cell.after(); row < tableNode.childCount; row++) {
+      const rowNode = tableNode.child(row);
       if(rowNode.childCount) {
         return rowStart + 1;
       } /* else -- rowNode has no children */
