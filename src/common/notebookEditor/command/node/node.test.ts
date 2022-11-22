32b258fa146b5a464f52c38d80f4b9fe538f4eaf
Alan Rodz <s061199@hotmail.com>
2022-11-22 13:20:32 -0600

    add tests

--- src/common/notebookEditor/command/node/node.test.ts
+++ src/common/notebookEditor/command/node/node.test.ts
@@ -19,6 +19,7 @@ const { bold: boldType } = notebookSchema.marks;
 
 const {
   [NodeName.BLOCKQUOTE]: blockquoteBuilder,
+  [NodeName.BULLET_LIST]: bulletListBuilder,
   [NodeName.CODEBLOCK]: codeBlockBuilder,
   [NodeName.DOC]: docBuilder,
   [NodeName.HEADING]: headingBuilder,
@@ -27,7 +28,7 @@ const {
   [NodeName.HORIZONTAL_RULE]: horizontalRuleBuilder,
   [NodeName.ORDERED_LIST]: orderedListBuilder,
   [NodeName.PARAGRAPH]: paragraphBuilder,
-} = getNotebookSchemaNodeBuilders([NodeName.BLOCKQUOTE, NodeName.CODEBLOCK, NodeName.DOC, NodeName.HEADING, NodeName.LIST_ITEM, NodeName.HORIZONTAL_RULE, NodeName.IMAGE, NodeName.ORDERED_LIST, NodeName.PARAGRAPH]);
+} = getNotebookSchemaNodeBuilders([NodeName.BLOCKQUOTE, NodeName.BULLET_LIST, NodeName.CODEBLOCK, NodeName.DOC, NodeName.HEADING, NodeName.LIST_ITEM, NodeName.HORIZONTAL_RULE, NodeName.IMAGE, NodeName.ORDERED_LIST, NodeName.PARAGRAPH]);
 
 const { [MarkName.BOLD]: boldBuilder, [MarkName.ITALIC]: italicBuilder } = getNotebookSchemaMarkBuilders([MarkName.BOLD, MarkName.ITALIC]);
 
@@ -64,7 +65,6 @@ describe('splitBlockCommand', () => {
     wrapTest(startState, splitBlockCommand, expectedEndState);
   });
 
-  // TODO: redefine and handle once Lists are added
   it('splits a parent Block when a Node is selected', () => {
     const startState = docBuilder(orderedListBuilder(listItemBuilder(paragraphBuilder('a')), `<${A}>`, listItemBuilder(paragraphBuilder('b')), listItemBuilder(paragraphBuilder('c'))));
     const expectedEndState = docBuilder(orderedListBuilder(listItemBuilder(paragraphBuilder('a'))), orderedListBuilder(listItemBuilder(paragraphBuilder('b')), listItemBuilder(paragraphBuilder('c'))));
@@ -159,12 +159,11 @@ describe('wrapInCommand', () => {
     wrapTest(startState, wrapInBlockquote, expectedEndState);
   });
 
-  // TODO: redefine and handle once Lists are added
-  // it('can wrap a Node Selection', () => {
-  //   const startState = docBuilder(`<${A}>`, unorderedListBuilder(listItemBuilder(paragraphBuilder('foo'))));
-  //   const expectedEndState = docBuilder(blockquoteBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('foo')))));
-  //   wrapTest(startState, wrapInBlockquote, expectedEndState);
-  // });
+  it('can wrap a Node Selection', () => {
+    const startState = docBuilder(`<${A}>`, bulletListBuilder(listItemBuilder(paragraphBuilder('foo'))));
+    const expectedEndState = docBuilder(blockquoteBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('foo')))));
+    wrapTest(startState, wrapInBlockquote, expectedEndState);
+  });
 });
 
 // -- Lift ------------------------------------------------------------------------
@@ -190,26 +189,26 @@ describe('liftCommand', () => {
 
   // TODO: redefine and handle once Lists are added
   // it('can lift out of a list', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder(`<${A}>foo`))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder(`<${A}>foo`))));
   //   const expectedEndState = docBuilder(paragraphBuilder('foo'));
   //   wrapTest(startState, liftCommand, expectedEndState);
   // });
 
   // it('lifts out of the innermost parent', () => {
-  //   const startState = docBuilder(blockquoteBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder(`foo<${A}>`)))));
+  //   const startState = docBuilder(blockquoteBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder(`foo<${A}>`)))));
   //   const expectedEndState = docBuilder(blockquoteBuilder(paragraphBuilder(`foo<${A}>`)));
   //   wrapTest(startState, liftCommand, expectedEndState);
   // });
 
   // it('can lift a node selection', () => {
-  //   const startState = docBuilder(blockquoteBuilder(`<${A}>`, unorderedListBuilder(listItemBuilder(paragraphBuilder('foo')))));
-  //   const expectedEndState = docBuilder(`<${A}>`, unorderedListBuilder(listItemBuilder(paragraphBuilder('foo'))));
+  //   const startState = docBuilder(blockquoteBuilder(`<${A}>`, bulletListBuilder(listItemBuilder(paragraphBuilder('foo')))));
+  //   const expectedEndState = docBuilder(`<${A}>`, bulletListBuilder(listItemBuilder(paragraphBuilder('foo'))));
   //   wrapTest(startState, liftCommand, expectedEndState);
   // });
 
   // it('lifts out of a nested list', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('one'), unorderedListBuilder(listItemBuilder(paragraphBuilder(`<${A}>sub1`)), listItemBuilder(paragraphBuilder('sub2')))), listItemBuilder(paragraphBuilder('two'))));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('one'), paragraphBuilder(`<${A}>sub1`), unorderedListBuilder(listItemBuilder(paragraphBuilder('sub2')))), listItemBuilder(paragraphBuilder('two'))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('one'), bulletListBuilder(listItemBuilder(paragraphBuilder(`<${A}>sub1`)), listItemBuilder(paragraphBuilder('sub2')))), listItemBuilder(paragraphBuilder('two'))));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('one'), paragraphBuilder(`<${A}>sub1`), bulletListBuilder(listItemBuilder(paragraphBuilder('sub2')))), listItemBuilder(paragraphBuilder('two'))));
   //   wrapTest(startState, liftCommand, expectedEndState);
   // });
 });
@@ -235,14 +234,14 @@ describe('liftEmptyBlockNodeCommand', () => {
 
   // TODO: redefine and handle once Lists are added
   // it('does not violate schema constraints', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder(`<${A}>foo`))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder(`<${A}>foo`))));
   //   const expectedEndState = null/*same state*/;
   //   wrapTest(startState, liftEmptyBlockNodeCommand, expectedEndState);
   // });
 
   // it('lifts out of a list', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder(`<${A}>`))));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi'))), paragraphBuilder());
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder(`<${A}>`))));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi'))), paragraphBuilder());
   //   wrapTest(startState, liftEmptyBlockNodeCommand, expectedEndState);
   // });
 });
@@ -281,37 +280,37 @@ describe('joinBackwardCommand', () => {
 
   // TODO: redefine and handle once Lists are added
   // it('moves a block into a list item', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi'))), paragraphBuilder(`<${A}>there`));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder('there'))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi'))), paragraphBuilder(`<${A}>there`));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder('there'))));
   //   wrapTest(startState, joinBackwardCommand, expectedEndState);
   // });
 
   // it('joins lists', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi'))), unorderedListBuilder(listItemBuilder(paragraphBuilder(`<${A}>there`))));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder('there'))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi'))), bulletListBuilder(listItemBuilder(paragraphBuilder(`<${A}>there`))));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder('there'))));
   //   wrapTest(startState, joinBackwardCommand, expectedEndState);
   // });
 
   // it('joins list items', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder(`<${A}>there`))));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi'), paragraphBuilder('there'))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder(`<${A}>there`))));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi'), paragraphBuilder('there'))));
   //   wrapTest(startState, joinBackwardCommand, expectedEndState);
   // });
 
   // it('lifts out of a list at the start', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder(`<${A}>there`))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder(`<${A}>there`))));
   //   const expectedEndState = docBuilder(paragraphBuilder(`<${A}>there`));
   //   wrapTest(startState, joinBackwardCommand, expectedEndState);
   // });
 
   // it('joins lists before and after', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi'))), paragraphBuilder(`<${A}>there`), unorderedListBuilder(listItemBuilder(paragraphBuilder('x'))));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder('there')), listItemBuilder(paragraphBuilder('x'))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi'))), paragraphBuilder(`<${A}>there`), bulletListBuilder(listItemBuilder(paragraphBuilder('x'))));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder('there')), listItemBuilder(paragraphBuilder('x'))));
   //   wrapTest(startState, joinBackwardCommand, expectedEndState);
   // });
 
   // it('does not return true on empty blocks that cannot be deleted', () => {
-  //   const startState = docBuilder(paragraphBuilder('a'), unorderedListBuilder(listItemBuilder(paragraphBuilder(`<${A}>`), unorderedListBuilder(listItemBuilder('b')))));
+  //   const startState = docBuilder(paragraphBuilder('a'), bulletListBuilder(listItemBuilder(paragraphBuilder(`<${A}>`), bulletListBuilder(listItemBuilder('b')))));
   //   const expectedEndState = null;
   //   wrapTest(startState, joinBackwardCommand, expectedEndState);
   // });
@@ -388,31 +387,31 @@ describe('joinForwardCommand', () => {
 
   // TODO: redefine and handle once Lists are added
   // it('pulls the next Block into the current ListItem', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder(`a<${A}>`)), listItemBuilder(paragraphBuilder('b'))));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('a'), paragraphBuilder('b'))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder(`a<${A}>`)), listItemBuilder(paragraphBuilder('b'))));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('a'), paragraphBuilder('b'))));
   //   wrapTest(startState, joinForwardCommand, expectedEndState);
   // });
 
   // it('joins two Blocks inside of a ListItem', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder(`a<${A}>`), paragraphBuilder('b'))));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('ab'))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder(`a<${A}>`), paragraphBuilder('b'))));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('ab'))));
   //   wrapTest(startState, joinForwardCommand, expectedEndState);
   // });
 
   // it('joins two lists', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder(`hi<${A}>`))), unorderedListBuilder(listItemBuilder(paragraphBuilder('there'))));
-  //   const expectedEndState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder('there'))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder(`hi<${A}>`))), bulletListBuilder(listItemBuilder(paragraphBuilder('there'))));
+  //   const expectedEndState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder('hi')), listItemBuilder(paragraphBuilder('there'))));
   //   wrapTest(startState, joinForwardCommand, expectedEndState);
   // });
 
   // it('does nothing in a nested node at the end of the document', () => {
-  //   const startState = docBuilder(unorderedListBuilder(listItemBuilder(paragraphBuilder(`there<${A}>`))));
+  //   const startState = docBuilder(bulletListBuilder(listItemBuilder(paragraphBuilder(`there<${A}>`))));
   //   const expectedEndState = null/*same state*/;
   //   wrapTest(startState, joinForwardCommand, expectedEndState);
   // });
 
   // it('does nothing when it cannot join', () => {
-  //   const startState = docBuilder(paragraphBuilder(`foo<${A}>`), unorderedListBuilder(listItemBuilder(paragraphBuilder('bar'), unorderedListBuilder(listItemBuilder(paragraphBuilder('baz'))))));
+  //   const startState = docBuilder(paragraphBuilder(`foo<${A}>`), bulletListBuilder(listItemBuilder(paragraphBuilder('bar'), bulletListBuilder(listItemBuilder(paragraphBuilder('baz'))))));
   //   const expectedEndState = null/*same state*/;
   //   wrapTest(startState, joinForwardCommand, expectedEndState);
   // });
