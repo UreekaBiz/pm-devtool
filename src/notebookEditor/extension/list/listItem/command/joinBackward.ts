db7087dc36fe9824504fc19657de5d4f557c9b18
Alan Rodz <s061199@hotmail.com>
2022-11-22 15:59:09 -0600

    add check

--- src/notebookEditor/extension/list/listItem/command/joinBackward.ts
+++ src/notebookEditor/extension/list/listItem/command/joinBackward.ts
@@ -1,6 +1,6 @@
-import { isBulletListNode, isOrderedListNode, JoinBackwardDocumentUpdate } from 'common';
-import { applyDocumentUpdates } from 'notebookEditor/command/update';
+import { isBlank, isBulletListNode, isOrderedListNode, JoinBackwardDocumentUpdate } from 'common';
 
+import { applyDocumentUpdates } from 'notebookEditor/command/update';
 import { Editor } from 'notebookEditor/editor/Editor';
 
 // ********************************************************************************
@@ -8,6 +8,7 @@ import { Editor } from 'notebookEditor/editor/Editor';
 export const joinBackwardToEndOfClosestListItem = (editor: Editor): boolean => {
   const { selection } = editor.view.state;
   const { empty, $from, from } = selection;
+  const { doc } = editor.view.state;
 
   if(!empty) return false/*do not allow if Selection is not empty*/;
   if(!$from.parent.isTextblock) return false/*parent is not a TextBlock, nothing to do*/;
@@ -17,13 +18,21 @@ export const joinBackwardToEndOfClosestListItem = (editor: Editor): boolean => {
         previousChildIndex = parentIndex -1;
   if(parentIndex === 0/*first direct child of Doc*/) return false/*no previous child by definition*/;
 
-  const previousChild = editor.view.state.doc.child(previousChildIndex);
+  const previousChild = doc.child(previousChildIndex);
   if(!isBulletListNode(previousChild) || isOrderedListNode(previousChild)) return false/*no List to join into*/;
 
-  let lastChildOfListPos = 0/*default*/;
-  previousChild.descendants((node, pos) => { lastChildOfListPos = pos; });
+  let lastChildOfList = doc/*default*/,
+      lastChildOfListPos = 0/*default*/;
+  previousChild.descendants((node, pos) => {
+    lastChildOfList = node;
+    lastChildOfListPos = pos;
+  });
 
-  const { depth: lastChildOfListDepth } = editor.view.state.doc.resolve(lastChildOfListPos);
+  let { depth: lastChildOfListDepth } = doc.resolve(lastChildOfListPos);
+  if(isBlank(lastChildOfList.textContent)/*no Text Nodes*/) {
+    lastChildOfListDepth += 1/*ensure result is firstChild of the last ListItem*/;
+  } /* else -- has Text content */
+
   const updates = [/*default empty*/];
   for(let i=0; i<=lastChildOfListDepth; i++) {
     updates.push(new JoinBackwardDocumentUpdate());
