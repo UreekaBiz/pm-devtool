3b4e171a588fec0404f5a83e3451aa12b45e2f96
Alan Rodz <s061199@hotmail.com>
2022-11-22 19:24:11 -0600

    change behavior

--- src/notebookEditor/extension/list/listItem/command/joinBackward.ts
+++ src/notebookEditor/extension/list/listItem/command/joinBackward.ts
@@ -1,14 +1,15 @@
-import { isBulletListNode, isOrderedListNode, JoinBackwardDocumentUpdate } from 'common';
+import { Transaction } from 'prosemirror-state';
 
-import { applyDocumentUpdates } from 'notebookEditor/command/update';
+import { isBulletListNode, isOrderedListNode, DocumentUpdate, JoinBackwardDocumentUpdate } from 'common';
+
 import { Editor } from 'notebookEditor/editor/Editor';
+import { applyDocumentUpdates } from 'notebookEditor/command/update';
 
 // ********************************************************************************
 // NOTE: this is a regular function since it calls applyDocumentUpdates
-export const joinBackwardToEndOfClosestListItem = (editor: Editor): boolean => {
-  const { selection } = editor.view.state;
+export const joinBackwardToEndOfClosestListItemCommand = (editor: Editor) => {
+  const { doc, selection } = editor.view.state;
   const { empty, $from, from } = selection;
-  const { doc } = editor.view.state;
 
   if(!empty) return false/*do not allow if Selection is not empty*/;
   if(!$from.parent.isTextblock) return false/*parent is not a TextBlock, nothing to do*/;
@@ -22,11 +23,22 @@ export const joinBackwardToEndOfClosestListItem = (editor: Editor): boolean => {
   if(!isBulletListNode(previousChild) || isOrderedListNode(previousChild)) return false/*no List to join into*/;
 
   let lastChildOfListPos = 0/*default*/;
-  previousChild.descendants((node, pos) => { lastChildOfListPos = pos; });
+  previousChild.descendants((node, pos) => { lastChildOfListPos = (pos+1/*inside the Node*/) + node.nodeSize; });
 
-  let { depth: lastChildOfListDepth } = doc.resolve(lastChildOfListPos);
-  const updates = [/*default empty*/];
-  for(let i=0; i<=lastChildOfListDepth; i++) { updates.push(new JoinBackwardDocumentUpdate()); }
+  let updatedState = editor.view.state,
+      updatedTr: Transaction | false = editor.view.state.tr;
+  let updateAmount = 0/*default*/;
 
+  // compute the required amount of times that Nodes must be JoinedBackward
+  while(updatedTr.selection.from !== lastChildOfListPos) {
+    updatedTr = new JoinBackwardDocumentUpdate().update(updatedState, updatedTr, editor.view);
+    if(updatedTr) {
+      updatedState = updatedState.apply(updatedTr);
+      updateAmount++;
+    } else { break/*could not join backward*/; }
+  }
+
+  const updates: DocumentUpdate[] = [];
+  for(let i=0; i<updateAmount; i++) { updates.push(new JoinBackwardDocumentUpdate()) }
   return applyDocumentUpdates(editor, updates);
 };
