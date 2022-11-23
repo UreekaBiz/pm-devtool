821c4fb0136752914e09789abbc6ac4842c7ed83
Alan Rodz <s061199@hotmail.com>
2022-11-23 13:29:19 -0600

    change checks

--- src/notebookEditor/extension/list/listItem/command/joinForward.ts
+++ src/notebookEditor/extension/list/listItem/command/joinForward.ts
@@ -1,4 +1,4 @@
-import { Command, EditorState, Selection, Transaction } from 'prosemirror-state';
+import { Command, EditorState, Selection, TextSelection, Transaction } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 
 import { isBulletListNode, isListItemNode, isOrderedListNode, AbstractDocumentUpdate } from 'common';
@@ -28,8 +28,12 @@ export class JoinForwardToStartOfClosestListItemDocumentUpdate implements Abstra
     if(!nextNodeIsList && !nextNodeIsListItem) return false/*do not handle*/;
 
     if($from.parent.content.size < 1/*empty*/) {
-      // do something else
-      return tr;
+      // if the current parent is empty, delete it up to the point where
+      // the next List or ListItem starts
+      const nextPosToRight = Selection.near(tr.doc.resolve(tr.selection.$from.end()+1), 1/*bias to the right*/);
+      tr.setSelection(TextSelection.create(tr.doc, tr.selection.from, nextPosToRight.$from.pos))
+        .deleteSelection();
+      return tr/*updated*/;
     } else {
       // move the contents of the next ListItem to the end of the current one
       const listItem = nodeAfterEnd.firstChild;
@@ -43,7 +47,8 @@ export class JoinForwardToStartOfClosestListItemDocumentUpdate implements Abstra
         const nextPosToRight = Selection.near(tr.doc.resolve(tr.selection.$from.end()+1/*past end of the current Block*/), 1/*bias to the right*/);
         tr.delete(tr.selection.from, nextPosToRight.from);
       } /* else -- no need to delete */
-      return tr;
+
+      return tr/*updated*/;
     }
   }
 }
