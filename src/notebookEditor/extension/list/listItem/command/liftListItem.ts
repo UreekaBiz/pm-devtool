b0bac28c01a80bf1622c1ff7969d9d9e2139a819
Alan Rodz <s061199@hotmail.com>
2022-11-24 12:01:28 -0600

    allow for single child lifting

--- src/notebookEditor/extension/list/listItem/command/liftListItem.ts
+++ src/notebookEditor/extension/list/listItem/command/liftListItem.ts
@@ -1,8 +1,8 @@
-import { Node as ProseMirrorNode } from 'prosemirror-model';
+import { Node as ProseMirrorNode, NodeRange } from 'prosemirror-model';
 import { Command, EditorState, Selection, Transaction } from 'prosemirror-state';
-import { liftTarget } from 'prosemirror-transform';
+import { findWrapping, liftTarget } from 'prosemirror-transform';
 
-import { isDocumentNode, isNotNullOrUndefined, AbstractDocumentUpdate, NodeGroup } from 'common';
+import { isDocumentNode, isListItemNode, AbstractDocumentUpdate, NodeGroup } from 'common';
 
 import { fromOrToInListItem, getListItemPositions } from './util';
 
@@ -43,32 +43,58 @@ export class LiftListItemDocumentUpdate implements AbstractDocumentUpdate {
 // perform the required modifications to a Transaction such that
 // the Item at the given position is lifted
 const liftListItem = (tr: Transaction, listItemPos: number) => {
-  const listItemBlockRange = tr.doc.resolve(listItemPos).blockRange();
-  if(!listItemBlockRange) return false/*cannot lift item, do not modify Transaction*/;
+  const listItem = tr.doc.nodeAt(listItemPos-1/*the ListItem itself*/),
+        $listItemPos = tr.doc.resolve(listItemPos),
+        list = $listItemPos.node(-1/*ancestor*/);
+  if(!listItem || !isListItemNode(listItem) || !list) return false/*cannot lift item, do not modify Transaction*/;
 
-  // try lift the contents of the ListItem
-  if(listItemBlockRange.depth) {
-    const targetDepth = liftTarget(listItemBlockRange);
-    if(!isNotNullOrUndefined<number>(targetDepth)) return false/*cannot perform lift operation*/;
+  const { $from } = tr.selection;
+  let liftedBlockRange: NodeRange | null = null/*default*/;
 
-    tr.lift(listItemBlockRange, targetDepth ? targetDepth : listItemBlockRange.depth - 1/*lift to parent*/);
-  } /* else -- range has depth 0 */
+  let liftedListItemChild = false/*default*/;
+  if($from.parent === listItem.firstChild) {
+    liftedBlockRange = $listItemPos.blockRange(/*lift the whole ListItem*/);
+  } else {
+    liftedBlockRange = $from.blockRange(/*lift the current child of the ListItem*/);
+    liftedListItemChild = true/*by definition*/;
+  }
+  if(!liftedBlockRange) return false/*no valid range to lift*/;
+
+  let targetDepth = liftTarget(liftedBlockRange);
+  if(liftedListItemChild) {
+    const wrappers = findWrapping(liftedBlockRange, list.type);
+    if(!wrappers) return false/*could not find valid wrapping when it was needed*/;
+
+    tr.wrap(liftedBlockRange, wrappers);
+
+    // update the lifted Range's end by creating a new one that includes the wrapping
+    const $from = tr.doc.resolve(liftedBlockRange.$from.pos),
+          $to = tr.doc.resolve(liftedBlockRange.$to.pos);
+    liftedBlockRange = new NodeRange($from, $to, liftedBlockRange.depth);
+  } /* else -- no need to wrap */
+
+  if(targetDepth) {
+    tr.lift(liftedBlockRange, targetDepth);
+  } else {
+    let liftDepth = liftedBlockRange.depth - (liftedListItemChild ? 2/*account for the wrapping*/ : 1 /*default just decrease depth by 1*/);
+    tr.lift(liftedBlockRange, liftDepth);
+  }
 
   // if the ListItem has depth 0 or its parent after lifting is not a List, delete the ListItem Range,
   // leaving only the content outside
-  const $listItemBlockStart = tr.doc.resolve(tr.mapping.map(listItemBlockRange.start));
-  if(!$listItemBlockStart.depth || !$listItemBlockStart.parent.type.spec.group?.includes(NodeGroup.LIST)) {
-    if(!$listItemBlockStart.nodeAfter) return false/*no node after the range's start*/;
+  const $liftedBlockRangeStart = tr.doc.resolve(tr.mapping.map(liftedBlockRange.start));
+  if(!$liftedBlockRangeStart.depth || !$liftedBlockRangeStart.parent.type.spec.group?.includes(NodeGroup.LIST)) {
+    const listItem = $liftedBlockRangeStart.nodeAfter;
+    if(!listItem) return false/*ListItem does not exist anymore*/;
     if(!tr.doc.type.contentMatch.defaultType) return false/*cannot insert a default NodeType at this position*/;
 
-    const listItem = $listItemBlockStart.nodeAfter,
-          listItemLiftedContent: ProseMirrorNode[] = [/*initially empty*/];
+    const listItemLiftedContent: ProseMirrorNode[] = [/*initially empty*/];
     for(let i=0; i<listItem.childCount; i++) {
       listItemLiftedContent.push(listItem.child(i));
     }
 
-    tr.replaceWith($listItemBlockStart.pos, $listItemBlockStart.pos + listItem.nodeSize, listItemLiftedContent)
-      .setSelection(Selection.near(tr.doc.resolve($listItemBlockStart.pos)));
+    tr.replaceWith($liftedBlockRangeStart.pos, $liftedBlockRangeStart.pos + listItem.nodeSize, listItemLiftedContent)
+      .setSelection(Selection.near(tr.doc.resolve($liftedBlockRangeStart.pos)));
   } /* else -- depth is defined or parent of range start is of type List */
 
   return tr;
