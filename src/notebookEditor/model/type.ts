1d73b3aea285b43ca45a09b2290b260ce8f274ac
Alan Rodz <s061199@hotmail.com>
2022-11-25 14:15:53 -0600

    change doc

--- src/notebookEditor/model/type.ts
+++ src/notebookEditor/model/type.ts
@@ -37,62 +37,82 @@ export enum ExtensionPriority {
    */
   MARK_HOLDER = 120,
 
-  // NOTE: Paragraph must have a higher priority than other block Nodes since it
-  //       is the 'default' block Node (by convention). If its priority is left
-  //       unspecified, the default block Node on document creation will be the
-  //       first block Node encountered in the editor Extension array
-  // SEE: notebookEditor/type.ts
+  /**
+   * Paragraph must have a higher priority than other block Nodes since it
+   * is the 'default' block Node (by convention). If its priority is left
+   * unspecified, the default block Node on document creation will be the
+   * first block Node encountered in the editor Extension array
+   *
+   * SEE: notebookEditor/type.ts
+   */
   PARAGRAPH = 119,
 
-  // NOTE: since the Plugins added by Tables handle Keydown and Mouse events,
-  //       they should run before other Plugins
+  /**
+   * since the Plugins added by Tables handle Keydown and Mouse events,
+   * they should run before other Plugins
+   */
   TABLE = 118,
 
-  // NOTE: Link must have a higher priority than other marks so that it gets
-  //       preference over them when creating, pasting or applying parse rules
+  /**
+   * Link must have a higher priority than other marks so that it gets
+   * preference over them when creating, pasting or applying parse rules
+   */
   LINK = 116/*T&E*/,
 
-  // NOTE: since Suggestions have specific behavior for the Enter and arrow
-  //       keydown events, they must run before other Extensions with these
-  //       keydown handlers run
+  /**
+   * since Suggestions have specific behavior for the Enter and arrow
+   * keydown events, they must run before other Extensions with these
+   * keydown handlers run
+   */
   EMOJI_SUGGESTION = 115,
 
-  // NOTE: asyncNodes must check if they are dirty after the codeBlocks have
-  //       been modified accordingly (e.g. codeBlockReferences and hashes) have
-  //       been recomputed. Hence this must run before other Extensions
+  /**
+   * asyncNodes must check if they are dirty after the codeBlocks have
+   * been modified accordingly (e.g. codeBlockReferences and hashes) have
+   * been recomputed. Hence this must run before other Extensions
+   */
   ASYNC_NODE = 114,
 
-  // NOTE: since Blocks make use of several specific Commands, they must not
-  //       be executed in default order
+  /**
+   * since Blocks make use of several specific Commands, they must not
+   * be executed in default order
+   */
   CODEBLOCK = 113,
 
-  // NOTE: (SEE: ExtensionPriority.CodeBlock)
+  /** (SEE: ExtensionPriority.CodeBlock) */
   DEMO_ASYNC_NODE_2 = 112,
 
-  // NOTE: (SEE: ExtensionPriority.CodeBlock)
-  // NOTE: since Blockquote shares the Mod-B keybinding with Bold, it must have
-  //       a priority higher than it so that it gets inserted without toggling it
+  /**
+   * (SEE: ExtensionPriority.CodeBlock)
+   * since Blockquote shares the Mod-B keybinding with Bold, it must have
+   * a priority higher than it so that it gets inserted without toggling it
+   */
   BLOCKQUOTE = 111,
 
-  // NOTE: since EditableInlineNodeWithContent shares the Mod-E keybinding
-  //       with Code, it must have a priority higher than it so that it
-  //       gets inserted without toggling it
+  /**
+   * since EditableInlineNodeWithContent shares the Mod-E keybinding
+   * with Code, it must have a priority higher than it so that it
+   * gets inserted without toggling it
+   */
   EDITABLE_INLINE_NODE_WITH_CONTENT = 110,
 
-  // NOTE: for consistency with EditableInlineNodeWithContent
+  /** for consistency with EditableInlineNodeWithContent */
   NESTED_VIEW_BLOCK_NODE = 109/*same as EditableInlineNodeWithContent*/,
 
-  // NOTE: since the Commands added by Lists handle Keydown,
-  //       they should run before other Plugins that also do so, but after
-  //       other TextBlock Nodes
+  /**
+   * since the Commands added by Lists handle Keydown,
+   * they should run before other Plugins that also do so, but after
+   * other TextBlock Nodes
+   */
   LIST_ITEM = 108,
 
-  // NOTE: since the Text Extension adds '\t' whenever Tab is pressed, but this
-  //       behavior is not always guaranteed to be the desired one (e.g. when
-  //       going through a list Node), the Text Extension runs last. This ensures
-  //       that the shortcuts defined in the Text Extension run only if their
-  //       trigger was not handled by another Extension previously
-  // SEE: NOTE above for default Extension priority
+  /**
+   * since the Text Extension adds '\t' whenever Tab is pressed, but this
+   * behavior is not always guaranteed to be the desired one (e.g. when
+   * going through a list Node), the Text Extension runs last. This ensures
+   * that the shortcuts defined in the Text Extension run only if their
+   * trigger was not handled by another Extension previously
+   */
   TEXT = 99,
 
   // -- Mark ----------------------------------------------------------------------
