fb123c2f4a0074fbfcb5aa6a79583943edd7b86d
Alan Rodz <s061199@hotmail.com>
2022-11-24 20:00:14 -0600

    checkpoint

--- src/notebookEditor/extension/list/listItem/command/util.ts
+++ src/notebookEditor/extension/list/listItem/command/util.ts
@@ -1,7 +1,8 @@
+import { NodeRange } from 'prosemirror-model';
 import { EditorState, Selection, Transaction } from 'prosemirror-state';
-import { canJoin } from 'prosemirror-transform';
+import { canJoin, findWrapping, liftTarget } from 'prosemirror-transform';
 
-import { isListItemNode, SelectionRange } from 'common';
+import { combineTransactionSteps, getTransformChangedRanges, isListItemNode, isListNode, SelectionRange } from 'common';
 
 // == Util ========================================================================
 /** get the positions of the start of the ListItems in the given Range */
@@ -87,3 +88,44 @@ export const checkAndMergeListAtPos = (tr: Transaction, posToCheck: number) => {
   return checkedAndMergedList;
 };
 
+/**
+ * prevent any ListItems that were changed by a set of Transactions
+ * from being loose (i.e. not inside a List)
+ */
+ export const checkAndLiftChangedLists = (transactions: readonly Transaction[], oldState: EditorState, tr: Transaction) => {
+  let wereListsLifted = false/*default*/;
+  const transform = combineTransactionSteps(oldState.doc, [...transactions]),
+        changes = getTransformChangedRanges(transform);
+  const singleRange = changes.reduce<{ from: number; to: number; }>((singleRange, nextChange) => {
+    const { newRange } = nextChange;
+    return { from: Math.min(singleRange.from, newRange.from), to: Math.max(singleRange.to, newRange.to) };
+  }, { from: tr.doc.nodeSize/*doc end*/, to: 0/*doc start*/ });
+
+  tr.doc.nodesBetween(singleRange.from, singleRange.to, (node, nodePos) => {
+    const $nodePos = tr.doc.resolve(nodePos + 1/*inside the Node*/),
+          parent = $nodePos.parent,
+          listItem = $nodePos.node(-1/*grandParent depth*/),
+          list = $nodePos.node(-2/*grandGrandParent depth*/);
+
+    // if the content at the nodePosition is 'loose' (i.e. not wrapped in)
+    // a List, wrap it, and lift it to the nearest List
+    if(listItem
+      && isListItemNode(listItem)
+      && list
+      && isListNode(list)
+      && !(parent === listItem.firstChild)
+      ) {
+      const blockRange = $nodePos.blockRange(),
+            wrappers = blockRange && findWrapping(blockRange, listItem.type);
+      if(!wrappers) return/*no wrapping possible, do nothing*/;
+
+      tr.wrap(blockRange, wrappers);
+      const updatedRange = new NodeRange(tr.doc.resolve(blockRange.$from.pos), tr.doc.resolve(blockRange.$to.pos), blockRange.depth),
+            liftTargetDepth = liftTarget(updatedRange);
+      liftTargetDepth && tr.lift(updatedRange, liftTargetDepth);
+      wereListsLifted = true/*lifted*/;
+    }
+  });
+
+  return wereListsLifted;
+};
--- src/notebookEditor/extension/list/listItem/plugin.ts
+++ src/notebookEditor/extension/list/listItem/plugin.ts
@@ -2,14 +2,15 @@ import { Plugin } from 'prosemirror-state';
 
 import { NoPluginState } from 'notebookEditor/model/type';
 
-import { checkAndMergeListAtPos } from './command';
+import { checkAndLiftChangedLists, checkAndMergeListAtPos } from './command';
 
 // ********************************************************************************
 // == Plugin ======================================================================
 export const listItemPlugin = () => new Plugin<NoPluginState>({
   // -- Transaction -------------------------------------------------------------
   // ensure that Lists are merged and joined if possible
-  // (SEE: checkAndMergeList)
+  // (SEE: checkAndMergeList), prevent any ListItems from
+  // being loose within a List (SEE: checkAndLiftChangedLists)
   appendTransaction(transactions, oldState, newState) {
     const { tr } = newState;
 
@@ -17,8 +18,9 @@ export const listItemPlugin = () => new Plugin<NoPluginState>({
     if(!wereListsMerged) {
       wereListsMerged = checkAndMergeListAtPos(tr, tr.selection.$from.after(1/*direct child of Doc depth*/));
     } /* else -- already merged */
+    const wereListsLifted = checkAndLiftChangedLists(transactions, oldState, tr);
 
-    if(wereListsMerged) {
+    if(wereListsMerged || wereListsLifted) {
       return tr/*modified*/;
     } /* else -- no Lists were merged */
 
