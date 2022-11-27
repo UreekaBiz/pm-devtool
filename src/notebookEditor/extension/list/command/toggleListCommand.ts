09052fefc5ab5492da151551b0f93a49e3f6eed2
Alan Rodz <s061199@hotmail.com>
2022-11-27 15:18:38 -0600

    change wrap behavior

--- src/notebookEditor/extension/list/command/toggleListCommand.ts
+++ src/notebookEditor/extension/list/command/toggleListCommand.ts
@@ -1,8 +1,6 @@
-import { Attrs, Fragment, NodeRange, NodeType, Slice } from 'prosemirror-model';
 import { Command, EditorState, Transaction } from 'prosemirror-state';
-import { canSplit, findWrapping, ReplaceAroundStep } from 'prosemirror-transform';
 
-import { findParentNodeClosestToPos, AbstractDocumentUpdate, Attributes, NodeName } from 'common';
+import { findParentNodeClosestToPos, AbstractDocumentUpdate, Attributes, NodeName, isDocumentNode, isListItemNode } from 'common';
 
 import { LiftListItemDocumentUpdate } from '../listItem/command';
 
@@ -16,7 +14,7 @@ export class ToggleListDocumentUpdate implements AbstractDocumentUpdate {
   /** modify the given Transaction such that a ListItem is lifted */
   public update(editorState: EditorState, tr: Transaction): Transaction | false {
     const { selection } = editorState,
-          { $from, $to } = selection;
+          { $from, from, $to, to } = selection;
 
     const listItemType = editorState.schema.nodes[NodeName.LIST_ITEM],
           listType = editorState.schema.nodes[this.listTypeName];
@@ -35,61 +33,24 @@ export class ToggleListDocumentUpdate implements AbstractDocumentUpdate {
         else /*change type */ { tr.setNodeMarkup(closestParentList.pos, listType, this.attrs); }
       } /* else - not nested, list does not exist, or cannot lift*/
     } else /*wrap*/ {
-      let outerRange = blockRange/*default*/,
-          performJoin = false/*default*/;
+      const insideDocBlockChildrenPositions: number[] = [/*default empty*/];
+      tr.doc.nodesBetween(from, to, (node, pos, parent, index) => {
+        if(parent && isDocumentNode(parent)) {
+          insideDocBlockChildrenPositions.push(pos+1/*inside the Child*/);
+        } /* else -- ignore */
+      });
 
-      if(blockRange.depth >= 2/*nested range*/
-        && $from.node(blockRange.depth-1/*depth of ancestor*/).type.compatibleContent(listItemType)
-        && blockRange.startIndex === 0/*first direct child*/
-      ) {
-        if($from.index(blockRange.depth - 1/*depth of ancestor*/) === 0) return false/*at the top of the List*/;
+      for(let i=0; i<insideDocBlockChildrenPositions.length; i++) {
+        const $pos = tr.doc.resolve(tr.mapping.map(insideDocBlockChildrenPositions[i]));
+        const nodeAtPos = tr.doc.nodeAt($pos.pos);
+        if(nodeAtPos && isListItemNode(nodeAtPos)) continue/*already wrapped*/;
 
-        let $insertionPos = editorState.doc.resolve(blockRange.start - 2/*account for start and end of range*/);
-        outerRange = new NodeRange($insertionPos, $insertionPos, blockRange.depth);
-
-        if(blockRange.endIndex < blockRange.parent.childCount) {
-          blockRange = new NodeRange($from, editorState.doc.resolve($to.end(blockRange.depth)), blockRange.depth);
-        } /* else -- the endIndex is not less than the childCount of the parent */
-        performJoin = true;
+        const blockRange = $pos.blockRange();
+        if(!blockRange) continue/*no range to wrap*/;
+        tr.wrap(blockRange, [{ type: listType, attrs: this.attrs }, { type: listItemType }]);
       }
-
-      const listWrapping = findWrapping(outerRange, listType, this.attrs, blockRange);
-      if(!listWrapping) return false/*no valid wrapping was found*/;
-      tr = applyListWrapping(tr, blockRange, listWrapping, performJoin, listType).scrollIntoView();
     }
 
     return tr/*updated*/;
   }
 }
-
-const applyListWrapping = (tr: Transaction, nodeRange: NodeRange, wrapperDefinitions: { type: NodeType; attrs?: Attrs | null; }[], performJoinBefore: boolean, listType: NodeType) => {
-  let newContent = Fragment.empty/*default*/;
-  for(let i = wrapperDefinitions.length - 1; i >= 0; i--) {
-    newContent = Fragment.from(wrapperDefinitions[i].type.create(wrapperDefinitions[i].attrs, newContent));
-  }
-
-  tr.step(new ReplaceAroundStep(nodeRange.start - (performJoinBefore ? 2/*account for start and end*/ : 0/*do not account*/), nodeRange.end, nodeRange.start, nodeRange.end,
-                                new Slice(newContent, 0/*use full Slice*/, 0/*use full Slice*/), wrapperDefinitions.length, true/*maintain structure*/));
-
-  let listWrapperAmount = 0;
-  for(let i = 0; i < wrapperDefinitions.length; i++) {
-    if(wrapperDefinitions[i].type === listType) {
-      listWrapperAmount = i + 1/*account for indexing*/;
-    } /* else -- ignore */
-  }
-
-  const splitDepth = wrapperDefinitions.length - listWrapperAmount;
-  let splitPosition = nodeRange.start + wrapperDefinitions.length - (performJoinBefore ? 2/*account for start and end*/ : 0/*do not account*/);
-
-  const { parent: nodeRangeParent } = nodeRange;
-  for(let nodeRangeIndex = nodeRange.startIndex, nodeRangeEndIndex = nodeRange.endIndex, firstIteration = true/*default*/; nodeRangeIndex < nodeRangeEndIndex; nodeRangeIndex++, firstIteration = false/*by definition*/) {
-    if(!firstIteration && canSplit(tr.doc, splitPosition, splitDepth)) {
-      tr.split(splitPosition, splitDepth);
-      splitPosition += (2 * splitDepth);
-    } /* else -- not the firstIteration or cannot split at splitPosition with splitDepth */
-
-    splitPosition += nodeRangeParent.child(nodeRangeIndex).nodeSize;
-  }
-
-  return tr/*modified*/;
-};
