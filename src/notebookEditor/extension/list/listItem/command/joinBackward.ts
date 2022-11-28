68ba319492fe9eb91293922ffd00744b3aa41fb5
Alan Rodz <s061199@hotmail.com>
2022-11-28 12:27:14 -0600

    simplify join backward

--- src/notebookEditor/extension/list/listItem/command/joinBackward.ts
+++ src/notebookEditor/extension/list/listItem/command/joinBackward.ts
@@ -1,6 +1,6 @@
-import { Command, EditorState, Transaction } from 'prosemirror-state';
+import { Command, EditorState, Selection, Transaction } from 'prosemirror-state';
 
-import { isListNode, AbstractDocumentUpdate } from 'common';
+import { AbstractDocumentUpdate } from 'common';
 
 // ********************************************************************************
 /**
@@ -18,29 +18,16 @@ export class JoinBackwardToEndOfClosestListItemDocumentUpdate implements Abstrac
    * Selection gets lifted to the closest ListItem above
    */
   public update(editorState: EditorState, tr: Transaction) {
-    const { doc, selection } = editorState,
+    const { selection } = editorState,
           { empty, $from, from } = selection,
           { parent } = $from;
     if(!empty) return false/*do not allow if Selection is not empty*/;
     if(!parent.isTextblock) return false/*parent is not a TextBlock, nothing to do*/;
     if($from.before() + 1/*inside the TextBlock*/ !== from) return false/*Selection is not at the start of the parent TextBlock*/;
 
-    const parentIndex = $from.index(0/*direct Doc depth*/),
-          previousChildIndex = parentIndex - 1/*by definition*/;
-    if(parentIndex === 0/*first direct child of Doc*/) return false/*no previous child by definition*/;
+    const newSelection = (Selection.near(tr.doc.resolve($from.before()), -1/*look backwards*/));
+    if(!(parent.type === newSelection.$from.parent.type)) return false/*cannot be merged*/;
 
-    const previousList = doc.child(previousChildIndex);
-    if(!isListNode(previousList)) return false/*no List to join into*/;
-
-    let lastTextChildOfListPos = 0/*default*/;
-    previousList.descendants((node, pos) => {
-      if(!node.isText) return/*ignore*/;
-      lastTextChildOfListPos = (pos + 1/*inside the Node*/) + node.nodeSize;
-    });
-
-    const $lastChildOfListPos = doc.resolve(lastTextChildOfListPos);
-    if(!(parent.type === $lastChildOfListPos.parent.type)) return false/*cannot be merged*/;
-
-    return tr.delete(lastTextChildOfListPos, from);
+    return tr.delete(newSelection.$from.pos, from);
   }
 }
