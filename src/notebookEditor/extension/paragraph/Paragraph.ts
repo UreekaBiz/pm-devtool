063b6636fe3e634cda2dd4e55d0132337c7cfff8
Alan Rodz <s061199@hotmail.com>
2022-11-29 16:01:13 -0600

    add checks

--- src/notebookEditor/extension/paragraph/Paragraph.ts
+++ src/notebookEditor/extension/paragraph/Paragraph.ts
@@ -1,6 +1,6 @@
 import { keymap } from 'prosemirror-keymap';
 
-import { getNodeOutputSpec, NodeName, ParagraphNodeSpec, DATA_NODE_TYPE } from 'common';
+import { getNodeOutputSpec, selectBlockNodeContentCommand, NodeName, ParagraphNodeSpec, DATA_NODE_TYPE } from 'common';
 
 import { shortcutCommandWrapper } from 'notebookEditor/command/util';
 import { ExtensionPriority } from 'notebookEditor/model/type';
@@ -36,5 +36,13 @@ export const Paragraph = new NodeExtension({
   pasteRules: (editor) => [/*none*/],
 
   // -- Plugin --------------------------------------------------------------------
-  addProseMirrorPlugins: (editor) => [keymap({ 'Mod-Alt-0': () => shortcutCommandWrapper(editor, setParagraphCommand) })],
+  addProseMirrorPlugins: (editor) => [
+    keymap({
+      'Mod-Alt-0': () => shortcutCommandWrapper(editor, setParagraphCommand),
+
+      // select all the content of the Paragraph
+      'Cmd-a': () => shortcutCommandWrapper(editor, selectBlockNodeContentCommand(NodeName.PARAGRAPH)),
+      'Cmd-A': () => shortcutCommandWrapper(editor, selectBlockNodeContentCommand(NodeName.PARAGRAPH)),
+    }),
+  ],
 });
