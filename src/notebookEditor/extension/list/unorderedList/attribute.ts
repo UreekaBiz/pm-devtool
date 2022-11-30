f3945ce26c0b9a13e6c406a824de4ee6cba28eaf
Alan Rodz <s061199@hotmail.com>
2022-11-30 12:00:41 -0600

    remove attrs

--- src/notebookEditor/extension/list/unorderedList/attribute.ts
+++
@@ -1,9 +0,0 @@
-import { UnorderedListAttributes } from 'common';
-
-import { NodeExtensionAttributes } from 'notebookEditor/extension/type/NodeExtension/type';
-
-// ********************************************************************************
-// == Attribute ===================================================================
-// NOTE: extracted so that it can be used by both the attributes of the Extension
-//       and added to all ParseRules of the parseDOM property
-export const UnorderedListAttrs: NodeExtensionAttributes<UnorderedListAttributes> = {/*currently no attrs*/};
