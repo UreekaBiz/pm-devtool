fb5e16684613382f45ee9f14247f24f6773abf42
Alan Rodz <s061199@hotmail.com>
2022-12-09 00:44:11 -0600

    checkpoint

--- src/notebookEditor/extension/codeblock/plugin.ts
+++ src/notebookEditor/extension/codeblock/plugin.ts
@@ -1,46 +1,23 @@
 import Prism from 'prismjs';
-import { EditorState, Plugin, PluginKey, Transaction } from 'prosemirror-state';
+import { Plugin, PluginKey } from 'prosemirror-state';
 import { Decoration, DecorationSet } from 'prosemirror-view';
 
-import { isCodeBlockNode, AttributeType } from 'common';
+import { isCodeBlockNode, AttributeType, CodeBlockNodeType } from 'common';
 
 import { Editor } from 'notebookEditor/editor/Editor';
+import { NoPluginState } from 'notebookEditor/model';
 
 import { getCodeBlockViewStorage, CodeBlockController } from './nodeView';
 
 // ********************************************************************************
 /** highlight the content of a CodeBlock given its language */
 
-// == Constant ====================================================================
-export const codeBlockPluginKey = new PluginKey<CodeBlockPluginState>('codeBlockPluginKey');
-
-// == Class =======================================================================
-export class CodeBlockPluginState {
-  constructor() {/*nothing additional*/}
-
-  // produce a new Plugin state
-  public apply = (tr: Transaction, thisPluginState: CodeBlockPluginState, oldEditorState: EditorState, newEditorState: EditorState) => {
-    return this/*state updated*/;
-  };
-}
-
 // == Plugin ======================================================================
-export const codeBlockPlugin = (editor: Editor) => new Plugin<CodeBlockPluginState>({
+export const codeBlockPlugin = (editor: Editor) => new Plugin<NoPluginState>({
     // -- Setup -------------------------------------------------------------------
-    key: codeBlockPluginKey,
-
-    // -- State -------------------------------------------------------------------
-    state: {
-      // initialize the plugin state
-      init: (_, state) => new CodeBlockPluginState(),
-
-      // apply changes to the plugin state from a view transaction
-      apply: (transaction, thisPluginState, oldState, newState) => thisPluginState.apply(transaction, thisPluginState, oldState, newState),
-    },
+    key: new PluginKey<NoPluginState>('codeBlockPluginKey'),
 
     // -- Props -------------------------------------------------------------------
-    // TODO: redefine as a Transaction Listener instead of a View Plugin, so that
-    //       the state is always the latest
     props: {
       decorations: (state) => {
         const codeBlockControllers: CodeBlockController[] = [/*default empty*/];
@@ -49,10 +26,11 @@ export const codeBlockPlugin = (editor: Editor) => new Plugin<CodeBlockPluginSta
 
         const decorations: Decoration[] = [/*default empty*/];
         for(let i=0; i<codeBlockControllers.length; i++) {
-          const codeBlock = state.doc.nodeAt(codeBlockControllers[i].getPos());
+          const codeBlockPos = codeBlockControllers[i].getPos(),
+                codeBlock = state.doc.nodeAt(codeBlockPos);
           if(!codeBlock || !isCodeBlockNode(codeBlock)) continue/*does not exist anymore*/;
 
-          const codeBlockDecorations = getDecorations(codeBlockControllers[i]);
+          const codeBlockDecorations = getDecorations(codeBlockPos, codeBlock);
           if(!codeBlockDecorations?.length) continue/*no decorations added*/;
 
           decorations.push(...codeBlockDecorations);
@@ -64,18 +42,17 @@ export const codeBlockPlugin = (editor: Editor) => new Plugin<CodeBlockPluginSta
   });
 
 // == Util ========================================================================
-const getDecorations = (codeBlockController: CodeBlockController) => {
+const getDecorations = (codeBlockPos: number, codeBlock: CodeBlockNodeType) => {
   const decorations: Decoration[] = [/*default empty*/];
-  const { node } = codeBlockController,
-        { [AttributeType.Language]: language } = node.attrs;
+  const { [AttributeType.Language]: language } = codeBlock.attrs;
   if(!language) return/*no language set in the CodeBlock*/;
 
   const prismGrammar = Prism.languages[language];
   if(!prismGrammar) return/*no prismGrammar exists for given language*/;
 
-  const tokenOrStrings = Prism.tokenize(node.textContent, prismGrammar);
+  const tokenOrStrings = Prism.tokenize(codeBlock.textContent, prismGrammar);
 
-  let absolutePos = codeBlockController.getPos() + 1/*skip the CodeBlock node itself*/;
+  let absolutePos = codeBlockPos + 1/*skip the CodeBlock node itself*/;
   for(let i=0; i<tokenOrStrings.length; i++) {
     const tokenOrString = tokenOrStrings[i];
     if(isPrismToken(tokenOrString)) {
