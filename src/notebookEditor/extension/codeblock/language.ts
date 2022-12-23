3528d4a5244d43134293cd778685b3b0bdb846e3
Alan Rodz <s061199@hotmail.com>
2022-12-23 10:26:03 -0600

    undo

--- src/notebookEditor/extension/codeblock/language.ts
+++ src/notebookEditor/extension/codeblock/language.ts
@@ -1,60 +1,42 @@
-import { format } from 'prettier';
-import cssParser from 'prettier/parser-postcss';
-import jsParser from 'prettier/parser-babel';
-import htmlParser from 'prettier/parser-html';
-import typeScriptParser from 'prettier/parser-typescript';
+import { parser as CSSHighlighter } from '@lezer/css';
+import { parser as HTMLHighlighter } from '@lezer/html';
+import { parser as jsHighlighter } from '@lezer/javascript';
+import prettier from 'prettier';
+import cssFormatter from 'prettier/parser-postcss';
+import jsFormatter from 'prettier/parser-babel';
+import htmlFormatter from 'prettier/parser-html';
+import typeScriptFormatter from 'prettier/parser-typescript';
 
 import { CodeBlockLanguage } from 'common';
 
 // ********************************************************************************
 const languageInfo = {
   [CodeBlockLanguage.CSS]: {
-    parserName: CodeBlockLanguage.CSS,
-    formatter: cssParser,
-    highlighter: cssParser.parsers.css,
+    formatter: cssFormatter,
+    highlighter: CSSHighlighter,
+    parser: CodeBlockLanguage.CSS,
   },
 
   [CodeBlockLanguage.HTML]: {
-    parserName: CodeBlockLanguage.HTML,
-    formatter: htmlParser,
-    highlighter: htmlParser.parsers.html,
+    formatter: htmlFormatter,
+    highlighter: HTMLHighlighter,
+    parser: CodeBlockLanguage.HTML,
   },
-
   [CodeBlockLanguage.JavaScript]: {
-    parserName: CodeBlockLanguage.JavaScript,
-    formatter: jsParser,
-    highlighter: jsParser.parsers.babel,
+    formatter: jsFormatter,
+    highlighter: jsHighlighter,
+    parser: 'babel',
   },
 
   [CodeBlockLanguage.TypeScript]: {
-    parserName: CodeBlockLanguage.TypeScript,
-    formatter: typeScriptParser,
-    highlighter: typeScriptParser.parsers.typescript,
+    formatter: typeScriptFormatter,
+    highlighter: jsHighlighter.configure({ dialect: 'ts' }),
+    parser: CodeBlockLanguage.TypeScript,
   },
 };
 export const formatCodeBlockChild = (codeBlockLanguage: CodeBlockLanguage, textContent: string) =>
-  format(textContent, { parser: languageInfo[codeBlockLanguage].parserName, plugins: [languageInfo[codeBlockLanguage].formatter] });
+  prettier.format(textContent, { parser: languageInfo[codeBlockLanguage].parser, plugins: [languageInfo[codeBlockLanguage].formatter] });
 
 // == Highlight ===================================================================
-export const getCodeBlockChildHighlightTree = (codeBlockLanguage: CodeBlockLanguage, textContent: string) => {
-  const { highlighter } = languageInfo[codeBlockLanguage];
-  try {
-    format(textContent, { parser: 'ppp', plugins: [myCustomPlugin] });
-  } catch(error) {
-    return null;
-  }
-};
-
-const myCustomPlugin = {
-  parsers: {
-    ['ppp']: {
-      parse(text: string) {
-        // @ts-ignore
-        const ast = cssParser.parsers.scss.parse(text);
-        console.log(ast);
-        return ast;
-      },
-      astFormat: "estree",
-    },
-  },
-};
+export const getCodeBlockChildHighlightTree = (codeBlockLanguage: CodeBlockLanguage, textContent: string) =>
+  languageInfo[codeBlockLanguage].highlighter.parse(textContent);
